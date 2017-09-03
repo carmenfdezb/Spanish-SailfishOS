d2715de9ade5ea08e8a6be88151fe8e195af58b1
Carmen F. B <6286776+carmenfdezb@users.noreply.github.com>
2017-09-03 23:30:27 +0200

    Add files via upload

---
+++ Lyrics/harbour-lyrics-es.ts
@@ -0,0 +1,68 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="es" version="2.1">
+<context>
+    <name>MainPage</name>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="76"/>
+        <source>Clear</source>
+        <translation>Limpiar</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="70"/>
+        <source>Settings</source>
+        <translation>Ajustes</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="179"/>
+        <source>Powered by %1</source>
+        <translation>Proporcionado por %1</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="119"/>
+        <source>Artist</source>
+        <translation>Artista</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="89"/>
+        <source>Copy to clipboard</source>
+        <translation>Copiar al portapapeles</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="131"/>
+        <source>Song</source>
+        <translation>Canción</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="148"/>
+        <source>Search</source>
+        <translation>Buscar</translation>
+    </message>
+</context>
+<context>
+    <name>Settings</name>
+    <message>
+        <location filename="../qml/pages/Settings.qml" line="39"/>
+        <source>Settings</source>
+        <translation>Ajustes</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Settings.qml" line="45"/>
+        <source>Provider</source>
+        <translation>Proveedor</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Settings.qml" line="66"/>
+        <source>Enable Media Player scanner</source>
+        <translation>Habilitar escaneo de Multimedia</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Settings.qml" line="67"/>
+        <source>Checks Media Player to get song info. Do not focus any field to allow text substitution.</source>
+        <translation>Consulta el reproductor Multimedia para obtener información de la canción. No te centres en ningún campo para permitir la sustitución del texto.</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Settings.qml" line="76"/>
+        <source>Clear cache</source>
+        <translation>Limpiar caché</translation>
+    </message>
+</context>
+</TS>
