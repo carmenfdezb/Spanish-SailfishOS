b5b3c82523d4b51db57dd939af8bcf0b61db0a52
Carmen F. B <6286776+carmenfdezb@users.noreply.github.com>
2017-09-24 11:11:37 +0200

    Add files via upload

---
+++ Piepmatz/harbour-piepmatz-es.ts
@@ -0,0 +1,440 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="es" version="2.1">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <source>About Piepmatz</source>
+        <translation>Acerca de Piepmatz</translation>
+    </message>
+    <message>
+        <source>A Twitter client for Sailfish OS</source>
+        <translation>Un cliente de Twitter para Sailfish OS</translation>
+    </message>
+    <message>
+        <source>By Sebastian J. Wolf</source>
+        <translation>De Sebastian J. Wolf</translation>
+    </message>
+    <message>
+        <source>Send E-Mail</source>
+        <translation>Enviar correo</translation>
+    </message>
+    <message>
+        <source>Licensed under GNU GPLv3</source>
+        <translation>Autorizado bajo GNU GPLv3</translation>
+    </message>
+    <message>
+        <source>Sources on GitHub</source>
+        <translation>Fuentes en GitHub</translation>
+    </message>
+    <message>
+        <source>Credits</source>
+        <translation>Créditos</translation>
+    </message>
+    <message>
+        <source>Open OAuth for Qt on GitHub</source>
+        <translation>Abrir OAuth para Qt en GitHub</translation>
+    </message>
+    <message>
+        <source>This project uses OAuth for Qt by Akos Polster. Thanks for making it available under the conditions of the BSD-2-Clause license!</source>
+        <translation>Este proyecto usa OAuth para Qt de Akos Polster. ¡Gracias por hacerlo posible bajo las condiciones de la licencia BSD de 2 cláusulas!</translation>
+    </message>
+    <message>
+        <source>This project uses twitter-text. Copyright 2014 Twitter, Inc. and other contributors. Thanks for making it available under the conditions of the Apache License, Version 2.0!</source>
+        <translation>Este proyecto usa twitter-text. Copyright 2014 Twitter, Inc. y otros colaboradores. ¡Gracias por hacerlo posible bajo las condiciones de la Licencia Apache, Versión 2.0!</translation>
+    </message>
+    <message>
+        <source>Open twitter-text on GitHub</source>
+        <translation>Abrir twitter-text en GitHub</translation>
+    </message>
+    <message>
+        <source>Find me on Twitter</source>
+        <translation>Búscame en Twitter</translation>
+    </message>
+    <message>
+        <source>About Twitter</source>
+        <translation>Acerca de Twitter</translation>
+    </message>
+    <message>
+        <source>Terms of Service</source>
+        <translation>Términos del servicio</translation>
+    </message>
+    <message>
+        <source>Privacy Policy</source>
+        <translation>Política de privacidad</translation>
+    </message>
+</context>
+<context>
+    <name>AttachImagesPage</name>
+    <message>
+        <source>Maximum number of images selected!</source>
+        <translation>¡Número máximo de imágenes seleccionadas!</translation>
+    </message>
+    <message>
+        <source>Attach %1 Images</source>
+        <translation>Adjuntar %1 imágenes</translation>
+    </message>
+    <message>
+        <source>Attach Image</source>
+        <translation>Adjuntar imagen</translation>
+    </message>
+    <message>
+        <source>Select Images</source>
+        <translation>Seleccionar imágenes</translation>
+    </message>
+</context>
+<context>
+    <name>ConversationPage</name>
+    <message>
+        <source>New message to %1</source>
+        <translation>Nuevo mensaje para %1</translation>
+    </message>
+    <message>
+        <source>%1 characters left</source>
+        <translation>Quedan %1 caracteres</translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>Loading...</source>
+        <translation>Cargando...</translation>
+    </message>
+</context>
+<context>
+    <name>FollowersPage</name>
+    <message>
+        <source>%1: Followers</source>
+        <translation>%1: Seguidores</translation>
+    </message>
+</context>
+<context>
+    <name>FriendsPage</name>
+    <message>
+        <source>%1: Following</source>
+        <translation>%1: Siguiendo</translation>
+    </message>
+</context>
+<context>
+    <name>ImageProgressIndicator</name>
+    <message>
+        <source>%1 %</source>
+        <translation>%1 %</translation>
+    </message>
+    <message>
+        <source>%1</source>
+        <translation>%1</translation>
+    </message>
+    <message>
+        <source>Error</source>
+        <translation>Error</translation>
+    </message>
+</context>
+<context>
+    <name>LoadingIndicator</name>
+    <message>
+        <source>Loading...</source>
+        <translation>Cargando...</translation>
+    </message>
+</context>
+<context>
+    <name>NewTweetPage</name>
+    <message>
+        <source>New Tweet</source>
+        <translation>Nuevo tuit</translation>
+    </message>
+    <message>
+        <source>Reply</source>
+        <translation>Responder</translation>
+    </message>
+    <message>
+        <source>Send Reply</source>
+        <translation>Enviar respuesta</translation>
+    </message>
+    <message>
+        <source>Send Tweet</source>
+        <translation>Enviar tuit</translation>
+    </message>
+    <message>
+        <source>Attach Images</source>
+        <translation>Adjuntar imágenes</translation>
+    </message>
+    <message>
+        <source>Retweet</source>
+        <translation>Retuitear</translation>
+    </message>
+    <message>
+        <source>Send Retweet</source>
+        <translation>Enviar retuit</translation>
+    </message>
+</context>
+<context>
+    <name>OverviewPage</name>
+    <message>
+        <source>Try again</source>
+        <translation>Intentar de nuevo</translation>
+    </message>
+    <message>
+        <source>Authenticate</source>
+        <translation>Autenticarse</translation>
+    </message>
+    <message>
+        <source>About Piepmatz</source>
+        <translation>Acerca de Piepmatz</translation>
+    </message>
+    <message>
+        <source>New Tweet</source>
+        <translation>Nuevo tuit</translation>
+    </message>
+    <message>
+        <source>Timeline</source>
+        <translation>Inicio</translation>
+    </message>
+    <message>
+        <source>Notifications</source>
+        <translation>Notificaciones</translation>
+    </message>
+    <message>
+        <source>Messages</source>
+        <translation>Mensajes</translation>
+    </message>
+    <message>
+        <source>Profile</source>
+        <translation>Perfil</translation>
+    </message>
+    <message>
+        <source>Search</source>
+        <translation>Buscar</translation>
+    </message>
+    <message>
+        <source>Refresh</source>
+        <translation>Actualizar</translation>
+    </message>
+    <message>
+        <source>Saying hello to Twitter...</source>
+        <translation>Di hola en Twitter...</translation>
+    </message>
+    <message>
+        <source>Loading timeline...</source>
+        <translation>Cargando inicio...</translation>
+    </message>
+    <message>
+        <source>Piepmatz could not log you in!</source>
+        <translation>¡Piepmatz no pudo iniciar sesión!</translation>
+    </message>
+    <message>
+        <source>Tweet sent successfully!</source>
+        <translation>¡Tuit enviado correctamente!</translation>
+    </message>
+    <message>
+        <source>Search on Twitter...</source>
+        <translation>Buscar en Twitter...</translation>
+    </message>
+    <message>
+        <source>No results found</source>
+        <translation>No hay resultados</translation>
+    </message>
+    <message>
+        <source>What are you looking for?</source>
+        <translation>¿Qué estás buscando?</translation>
+    </message>
+    <message>
+        <source>Sending tweet...</source>
+        <translation>Enviando tuit...</translation>
+    </message>
+    <message>
+        <source>Uploading, %1% completed...</source>
+        <translation>Subiendo, %1% completado...</translation>
+    </message>
+    <message>
+        <source>You</source>
+        <translation>Tú</translation>
+    </message>
+    <message>
+        <source>No direct messages in the last 30 days</source>
+        <translation>No hay mensajes directos en los últimos 30 días</translation>
+    </message>
+    <message>
+        <source>Tweets</source>
+        <translation>Tuits</translation>
+    </message>
+    <message>
+        <source>Users</source>
+        <translation>Usuarios</translation>
+    </message>
+    <message>
+        <source>Searching...</source>
+        <translation>Buscando...</translation>
+    </message>
+    <message>
+        <source>New Mentions</source>
+        <translation>Nuevas menciones</translation>
+    </message>
+    <message>
+        <source>You have been mentioned!</source>
+        <translation>¡Te han mencionado!</translation>
+    </message>
+    <message>
+        <source>New Messages</source>
+        <translation>Nuevos mensajes</translation>
+    </message>
+    <message>
+        <source>You have new direct messages!</source>
+        <translation>¡Tienes nuevos mensajes directos!</translation>
+    </message>
+    <message>
+        <source>You have new messages!</source>
+        <translation>¡Tienes nuevos mensajes!</translation>
+    </message>
+    <message>
+        <source>Load more tweets</source>
+        <translation>Cargar más tuits</translation>
+    </message>
+    <message>
+        <source>No tweets found. Follow more people to get their tweets in your timeline!</source>
+        <translation>No hay tuits. ¡Sigue a más gente para ver sus tuits en la página de inicio!</translation>
+    </message>
+</context>
+<context>
+    <name>Profile</name>
+    <message>
+        <source>%1 Following</source>
+        <translation>%1 Siguiendo</translation>
+    </message>
+    <message>
+        <source>%1 Followers</source>
+        <translation>%1 Seguidores</translation>
+    </message>
+    <message>
+        <source>%1 Tweets</source>
+        <translation>%1 Tuits</translation>
+    </message>
+    <message>
+        <source>Joined in %1</source>
+        <translation>Se unió en %1</translation>
+    </message>
+</context>
+<context>
+    <name>ProfileHeader</name>
+    <message>
+        <source>@%1</source>
+        <translation>@%1</translation>
+    </message>
+</context>
+<context>
+    <name>ProfilePage</name>
+    <message>
+        <source>Unfollow %1</source>
+        <translation>Dejar de seguir a %1</translation>
+    </message>
+    <message>
+        <source>Follow %1</source>
+        <translation>Seguir a %1</translation>
+    </message>
+    <message>
+        <source>You follow %1 now.</source>
+        <translation>Ahora sigues a %1.</translation>
+    </message>
+    <message>
+        <source>You don&apos;t follow %1 anymore.</source>
+        <translation>Ya no sigues a %1.</translation>
+    </message>
+    <message>
+        <source>Send Direct Message</source>
+        <translation>Enviar mensaje directo</translation>
+    </message>
+</context>
+<context>
+    <name>TextPage</name>
+    <message>
+        <source>Terms of Service</source>
+        <translation>Términos del servicio</translation>
+    </message>
+    <message>
+        <source>Privacy Policy</source>
+        <translation>Política de privacidad</translation>
+    </message>
+</context>
+<context>
+    <name>Tweet</name>
+    <message>
+        <source>Retweeted by %1</source>
+        <translation>Retuiteado por %1</translation>
+    </message>
+    <message>
+        <source>In reply to %1</source>
+        <translation>En respuesta a %1</translation>
+    </message>
+    <message>
+        <source>Open in Browser</source>
+        <translation>Abrir en el navegador</translation>
+    </message>
+    <message>
+        <source>Reply to Tweet</source>
+        <translation>Responder tuit</translation>
+    </message>
+    <message>
+        <source>Retweet with Comment</source>
+        <translation>Retuitear con comentario</translation>
+    </message>
+</context>
+<context>
+    <name>TweetUser</name>
+    <message>
+        <source>@%1</source>
+        <translation>@%1</translation>
+    </message>
+</context>
+<context>
+    <name>WelcomePage</name>
+    <message>
+        <source>Welcome to Piepmatz!</source>
+        <translation>¡Bienvenido a Piepmatz!</translation>
+    </message>
+    <message>
+        <source>Please login to Twitter to continue.</source>
+        <translation>Por favor, inicia sesión en Twitter para continuar.</translation>
+    </message>
+    <message>
+        <source>Log in to Twitter</source>
+        <translation>Iniciar sesión en Twitter</translation>
+    </message>
+    <message>
+        <source>If you don&apos;t have a Twitter account yet, please sign up first.</source>
+        <translation>Si aún no tienes cuenta de Twitter, por favor, regístrate primero.</translation>
+    </message>
+    <message>
+        <source>Sign up for Twitter</source>
+        <translation>Registrarse en Twitter</translation>
+    </message>
+    <message>
+        <source>OK</source>
+        <translation>OK</translation>
+    </message>
+    <message>
+        <source>Please enter the Twitter PIN:</source>
+        <translation>Por favor, introduce el pin de Twitter:</translation>
+    </message>
+    <message>
+        <source>Unable to authenticate you with the entered PIN.</source>
+        <translation>No es posible autenticarse con el PIN introducido.</translation>
+    </message>
+    <message>
+        <source>Enter PIN again</source>
+        <translation>Introduce el PIN de nuevo</translation>
+    </message>
+    <message>
+        <source>Restart authentication</source>
+        <translation>Reiniciar autenticación</translation>
+    </message>
+</context>
+<context>
+    <name>functions</name>
+    <message>
+        <source>#%1</source>
+        <translation>#%1</translation>
+    </message>
+    <message>
+        <source>@%1</source>
+        <translation>@%1</translation>
+    </message>
+</context>
+</TS>
