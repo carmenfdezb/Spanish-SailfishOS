69eb31ee361e87dd05c81d5fbd02d3d5e0ef81f5
Carmen F. B <6286776+carmenfdezb@users.noreply.github.com>
2017-09-24 12:09:42 +0200

    Add files via upload

---
+++ BikeMe/harbour-bikeme-es.ts
@@ -0,0 +1,250 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="es" version="2.1">
+<context>
+    <name>About</name>
+    <message>
+        <source>About</source>
+        <translation>Sobre</translation>
+    </message>
+    <message>
+        <source>Find a bike in your city!</source>
+        <translation>¡Encuentra una bicicleta en tu ciudad!</translation>
+    </message>
+    <message>
+        <source>Version %1</source>
+        <translation>Versión %1</translation>
+    </message>
+    <message>
+        <source>Many thanks for the translations: %1</source>
+        <translation>Muchas gracias por las traducciones: %1</translation>
+    </message>
+    <message>
+        <source>This app helps you to find a bike or a free parking spot for various self-service bicycle schemes arround the world:&lt;ul&gt;%1&lt;li&gt;And more!&lt;/li&gt;&lt;/ul&gt;BikeMe provides a simple interactive map displaying all stations by city, their number of available bikes and free parking spots, and the geolocation.&lt;br /&gt;Click on %2 to activate the geolocation and on %3 to center the map on your position.&lt;br/&gt;Regular users: Save your favourites stations and see their status directly on the cover!</source>
+        <translation>Esta aplicación te permite encontrar una bicicleta o un lugar libre para estacionarla a través de varios sistemas de bicicleta de libre servicio que existen en el mundo:&lt;ul&gt;%1&lt;li&gt;¡Y varios otros más!&lt;/li&gt;&lt;/ul&gt;BikeMe te proporciona un mapa que muestra las diferentes estaciones para cada ciudad, así como el número de bicicletas y de lugares disponibles para estacionarlas. De igual forma, te permite geolocalizarte.&lt;br /&gt;Haz clic en %2 para activar la geolocalización y en %3 para centrar el mapa en tu posición.&lt;br/&gt;Usuarios frecuentes: ¡guarda tus estaciones favoritas y visualiza sus estados directamente en la cubierta!</translation>
+    </message>
+</context>
+<context>
+    <name>CitiesList</name>
+    <message>
+        <source>Fetching operators...</source>
+        <translation>Descargando ciudades...</translation>
+    </message>
+    <message>
+        <source>Cities</source>
+        <translation>Ciudades</translation>
+    </message>
+    <message>
+        <source>Filter</source>
+        <translation>Filtrar</translation>
+    </message>
+    <message>
+        <source>Welcome to BikeMe!</source>
+        <translation>¡Bienvenido a BikeMe!</translation>
+    </message>
+    <message>
+        <source>Pull down to load cities list</source>
+        <translation>Desliza hacia abajo para cargar la lista de ciudades</translation>
+    </message>
+    <message>
+        <source>About BikeMe</source>
+        <translation>Sobre BikeMe</translation>
+    </message>
+    <message>
+        <source>Update cities list</source>
+        <translation>Actualizar la lista de ciudades</translation>
+    </message>
+    <message>
+        <source>Select data source</source>
+        <translation>Seleccionar fuente de datos</translation>
+    </message>
+</context>
+<context>
+    <name>Favourites</name>
+    <message>
+        <source>Updated: %1</source>
+        <translation>Hace: %1</translation>
+    </message>
+    <message>
+        <source>Closed</source>
+        <translation>Cerrado</translation>
+    </message>
+    <message>
+        <source>Move Up</source>
+        <translation>Mover hacia arriba</translation>
+    </message>
+    <message>
+        <source>Move Down</source>
+        <translation>Mover hacia abajo</translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation>Borrar</translation>
+    </message>
+    <message>
+        <source>Deleting</source>
+        <translation>Borrando</translation>
+    </message>
+    <message>
+        <source>Add a new favourite</source>
+        <translation>Añadir nueva estación a favoritos</translation>
+    </message>
+    <message>
+        <source>Adding a favourite</source>
+        <translation>Añadiendo favorito</translation>
+    </message>
+    <message>
+        <source>Station %1 does not exist</source>
+        <translation>La estación %1 no existe</translation>
+    </message>
+    <message>
+        <source>Favourites - %1</source>
+        <translation>Favoritos - %1</translation>
+    </message>
+    <message>
+        <source>Update all</source>
+        <translation>Actualizar todo</translation>
+    </message>
+    <message>
+        <source>Update</source>
+        <translation>Actualizar</translation>
+    </message>
+    <message>
+        <source>Individual update not available for this city</source>
+        <translation>La actualización de cada estación no está disponible para esta ciudad</translation>
+    </message>
+    <message>
+        <source>No favourites yet</source>
+        <translation>Aún sin favoritos</translation>
+    </message>
+    <message>
+        <source>You can add your favourites from the map or using the field below if you know the station number.</source>
+        <translation>Puedes añadir tus favoritos desde el mapa o utilizar el campo de texto si conoces el número de la estación.</translation>
+    </message>
+</context>
+<context>
+    <name>InteractiveMap</name>
+    <message>
+        <source>Updated: %1</source>
+        <translation>Actualizado: %1</translation>
+    </message>
+    <message>
+        <source>Updating...</source>
+        <translation>Actualizando...</translation>
+    </message>
+    <message>
+        <source>Updated:</source>
+        <translation>Actualizado:</translation>
+    </message>
+    <message>
+        <source>Loading stations...</source>
+        <translation>Cargando estaciones...</translation>
+    </message>
+    <message>
+        <source>Can&apos;t display all status for this city.</source>
+        <translation>No es posible mostrar todos los estados para esta ciudad.</translation>
+    </message>
+    <message>
+        <source>Only &quot;all status&quot; mode available for this city.</source>
+        <translation>Solo el modo &quot;todos los estados&quot; está disponible para esta ciudad.</translation>
+    </message>
+</context>
+<context>
+    <name>ProgressInfoBar</name>
+    <message numerus="yes">
+        <source>%n Cities</source>
+        <translation><numerusform>%n Ciudad</numerusform><numerusform>%n Ciudades</numerusform></translation>
+    </message>
+    <message numerus="yes">
+        <source>%n Error(s)</source>
+        <translation><numerusform>%n Error</numerusform><numerusform>%n Errores</numerusform></translation>
+    </message>
+</context>
+<context>
+    <name>SecondPage</name>
+    <message>
+        <source>Settings</source>
+        <translation>Ajustes</translation>
+    </message>
+    <message>
+        <source>Stations Map</source>
+        <translation>Mapa de estaciones</translation>
+    </message>
+    <message>
+        <source>Favourites</source>
+        <translation>Favoritos</translation>
+    </message>
+</context>
+<context>
+    <name>SelectDataSource</name>
+    <message>
+        <source>Data source</source>
+        <translation>Fuente de datos</translation>
+    </message>
+    <message>
+        <source>Cities list must be updated after changing data source</source>
+        <translation>La lista de ciudades debe actualizarse después de cambiar la fuente de datos</translation>
+    </message>
+    <message>
+        <source>Official data</source>
+        <translation>Datos oficiales</translation>
+    </message>
+    <message>
+        <source>BikeMe uses data directly from bikes providers as much as possible, but a lot of them don&apos;t expose their data, so it&apos;s a tedious work to support those cities.&lt;br&gt;A project is doing that work very well: &lt;a href=&quot;https://citybik.es&quot;&gt;CityBikes&lt;/a&gt;. So if your city is not available in the &quot;Official data&quot;, check out if it is in the data from CityBikes!</source>
+        <translation>BikeMe utiliza directamente los datos de los proveedores de bicicletas siempre que sea posible, pero muchos proveedores no ponen a disposición sus datos, por lo tanto para incluir estas ciudades se necesita mucho trabajo.&lt;br&gt;Sin embargo, otro projecto está realizando este trabajo bastante bien: &lt;a href=&quot;https://citybik.es&quot;&gt;CityBikes&lt;/a&gt;. Entonces si tu ciudad no se encuentra en los &quot;Datos oficiales&quot;, ¡comprueba si está en los datos de CityBikes!</translation>
+    </message>
+</context>
+<context>
+    <name>Settings</name>
+    <message>
+        <source>Settings</source>
+        <translation>Ajustes</translation>
+    </message>
+    <message>
+        <source>Map provider</source>
+        <translation>Proveedor del mapa</translation>
+    </message>
+    <message>
+        <source>Start page</source>
+        <translation>Página de inicio</translation>
+    </message>
+    <message>
+        <source>Clear cache</source>
+        <translation>Borrar caché</translation>
+    </message>
+    <message>
+        <source>BikeMe keeps in cache stations location to save mobile data. If stations don&apos;t display, you can try to clear the cache to remove potentially corrupted files.</source>
+        <translation>BikeMe guarda en caché la posición de las estaciones para evitar el consumo de datos móviles. Si las estaciones no se muestran, borra la memoria caché para suprimir los posibles archivos incompletos.</translation>
+    </message>
+    <message>
+        <source>Display all stations status</source>
+        <translation>Mostrar el estado de todas las estaciones</translation>
+    </message>
+    <message>
+        <source>All station status will be downloaded and displayed on the map. Consume more data.</source>
+        <translation>Descargar los estados de todas las estaciones. Consumirá más datos.</translation>
+    </message>
+    <message>
+        <source>Default</source>
+        <translation>Por defecto</translation>
+    </message>
+    <message>
+        <source>Map</source>
+        <translation>Mapa</translation>
+    </message>
+    <message>
+        <source>Favourites</source>
+        <translation>Favoritos</translation>
+    </message>
+    <message>
+        <source>Clearing cache</source>
+        <translation>Borrando caché</translation>
+    </message>
+    <message>
+        <source>When opening the map, the GPS will be already enabled and the map will center on your position.</source>
+        <translation>Al abrir el mapa, el GPS ya estará activado y el mapa se centrará en tu posición.</translation>
+    </message>
+    <message>
+        <source>Open map on my position</source>
+        <translation>Abrir mapa en tu posición</translation>
+    </message>
+</context>
+</TS>
