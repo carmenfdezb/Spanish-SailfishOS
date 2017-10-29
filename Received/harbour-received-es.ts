9925f944a48c805f79c18056b5684d7a54c476fe
Carmen F. B <6286776+carmenfdezb@users.noreply.github.com>
2017-10-29 11:11:13 +0100

    Add files via upload

---
+++ Received/harbour-received-es.ts
@@ -0,0 +1,185 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="es_ES">
+<context>
+    <name>BrowseByCategoryPageForm.ui</name>
+    <message>
+        <source>Browse by</source>
+        <translation>Explorar por</translation>
+    </message>
+</context>
+<context>
+    <name>BrowsePage</name>
+    <message>
+        <source>Local</source>
+        <translation>Local</translation>
+    </message>
+    <message>
+        <source>Top 100</source>
+        <translation>Top 100</translation>
+    </message>
+    <message>
+        <source>Recommended</source>
+        <translation>Recomendado</translation>
+    </message>
+    <message>
+        <source>Genre</source>
+        <translation>Género</translation>
+    </message>
+    <message>
+        <source>Topic</source>
+        <translation>Tema</translation>
+    </message>
+    <message>
+        <source>Country</source>
+        <translation>País</translation>
+    </message>
+    <message>
+        <source>City</source>
+        <translation>Ciudad</translation>
+    </message>
+    <message>
+        <source>Language</source>
+        <translation>Idioma</translation>
+    </message>
+</context>
+<context>
+    <name>BrowsePageForm.ui</name>
+    <message>
+        <source>Browse</source>
+        <translation>Explorar</translation>
+    </message>
+</context>
+<context>
+    <name>DockedAudioPlayerForm.ui</name>
+    <message>
+        <source>Sleep timer</source>
+        <translation>Temporizador</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Cancelar</translation>
+    </message>
+    <message>
+        <source>Set</source>
+        <translation>Configurar</translation>
+    </message>
+</context>
+<context>
+    <name>FavoritePageForm.ui</name>
+    <message>
+        <source>http://mystation.com/stream.mp3</source>
+        <translation>http://miestacion.com/stream.mp3</translation>
+    </message>
+    <message>
+        <source>URL</source>
+        <translation>URL</translation>
+    </message>
+    <message>
+        <source>My Station</source>
+        <translation>Mi estación</translation>
+    </message>
+    <message>
+        <source>Name</source>
+        <translation>Nombre</translation>
+    </message>
+    <message>
+        <source>http://mystation.com/logo.jpg</source>
+        <translation>http://miestacion.com/logo.jpg</translation>
+    </message>
+    <message>
+        <source>Logo</source>
+        <translation>Logo</translation>
+    </message>
+    <message>
+        <source>Sweden</source>
+        <translation>España</translation>
+    </message>
+    <message>
+        <source>Country</source>
+        <translation>País</translation>
+    </message>
+</context>
+<context>
+    <name>FavoritesListContextMenu</name>
+    <message>
+        <source>Remove from favorite</source>
+        <translation>Eliminar de favoritos</translation>
+    </message>
+    <message>
+        <source>Edit favorite</source>
+        <translation>Editar</translation>
+    </message>
+</context>
+<context>
+    <name>FavoritesPageForm.ui</name>
+    <message>
+        <source>Favorites</source>
+        <translation>Favoritos</translation>
+    </message>
+    <message>
+        <source>Add Custom</source>
+        <translation>Añadir estación persanalizada</translation>
+    </message>
+</context>
+<context>
+    <name>NavigationMenuForm.ui</name>
+    <message>
+        <source>Settings</source>
+        <translation>Ajustes</translation>
+    </message>
+    <message>
+        <source>Show player</source>
+        <translation>Mostrar reproductor</translation>
+    </message>
+    <message>
+        <source>Browse</source>
+        <translation>Explorar</translation>
+    </message>
+    <message>
+        <source>Search</source>
+        <translation>Buscar</translation>
+    </message>
+</context>
+<context>
+    <name>StationsListContextMenu</name>
+    <message>
+        <source>Remove from favorite</source>
+        <translation>Eliminar de favoritos</translation>
+    </message>
+    <message>
+        <source>Add to favorite</source>
+        <translation>Añadir a favoritos</translation>
+    </message>
+</context>
+<context>
+    <name>StationsPageForm.ui</name>
+    <message>
+        <source>From</source>
+        <translation>De</translation>
+    </message>
+</context>
+<context>
+    <name>Utils</name>
+    <message>
+        <source>Search station</source>
+        <translation>Buscar estación</translation>
+    </message>
+    <message>
+        <source>Top 100</source>
+        <translation>Top 100</translation>
+    </message>
+    <message>
+        <source>Favorites</source>
+        <translation>Favoritos</translation>
+    </message>
+    <message>
+        <source>Recommended</source>
+        <translation>Recomendado</translation>
+    </message>
+    <message>
+        <source>Local</source>
+        <translation>Local</translation>
+    </message>
+</context>
+</TS>
