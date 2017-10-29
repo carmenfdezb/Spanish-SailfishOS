3c9f0b6ee6e51ccb0bde2b23bb1a281aa51d3556
Carmen F. B <6286776+carmenfdezb@users.noreply.github.com>
2017-10-29 19:09:53 +0100

    Add files via upload

---
+++ Sailgoal/harbour-sailgoal-es.ts
@@ -0,0 +1,151 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="es_ES">
+<context>
+    <name>About</name>
+    <message>
+        <location filename="../qml/pages/About.qml" line="12"/>
+        <source>About</source>
+        <translation>Acerca de</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="19"/>
+        <source>Created by: </source>
+        <translation>Creado por: </translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="37"/>
+        <source>About the application:</source>
+        <translation>Acerca de la aplicación:</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="47"/>
+        <source>This application shows results and standings of european football leagues. Use the pulley menus to navigate. Feel free to write bug reports and feature requests in the comments on openrepos.net</source>
+        <translation>Esta aplicación muestra los resultados y clasificaciones de las ligas europeas de fútbol. Usa el menú deslizante para navegar. No dudes en enviar errores o solicitar nuevas funciones en los comentarios de Openrepos.net.</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="57"/>
+        <source>Thanks to: </source>
+        <translation>Agradecimientos: </translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="82"/>
+        <source>Used icons under cc-licence:</source>
+        <translation>Iconos usados bajo licencia CC:</translation>
+    </message>
+    <message>
+        <source>created by: </source>
+        <translation type="vanished">creado por:</translation>
+    </message>
+    <message>
+        <source>about the application</source>
+        <translation type="vanished">acerca de la aplicación</translation>
+    </message>
+    <message>
+        <source>This application shows results and standings of european football leagues. Use the pulley menus to navigate and click on the header of the first page to change the league. Feel free to write bug reports and feature requests in the comments on openrepos.net</source>
+        <translation type="vanished">Esta aplicación muestra los resultados y clasificaciones de las ligas europeas de fútbol. Usa el menú deslizante para navegar y haz clic en la cabecera de la página principal para cambiar de liga. No dudes en enviar errores o solicitar nuevas funciones en los comentarios de Openrepos.net.</translation>
+    </message>
+    <message>
+        <source>thanks to: </source>
+        <translation type="vanished">gracias a:</translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <location filename="../qml/cover/CoverPage.qml" line="35"/>
+        <source>No live games at the moment</source>
+        <translation>No hay partidos en juego</translation>
+    </message>
+</context>
+<context>
+    <name>FirstPage</name>
+    <message>
+        <source>Matchday</source>
+        <translation type="vanished">Jornada</translation>
+    </message>
+    <message>
+        <source>Next Matchday</source>
+        <translation type="vanished">Próxima jornada</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="111"/>
+        <source>About</source>
+        <translation>Acerca de</translation>
+    </message>
+    <message>
+        <source>Select Matchday</source>
+        <translation type="vanished">Seleccionar jornada</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="72"/>
+        <source>Change League</source>
+        <translation>Cambiar liga</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="85"/>
+        <source>Standings</source>
+        <translation>Clasificación</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="92"/>
+        <source>Update</source>
+        <translation>Actualizar</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="102"/>
+        <source>Timer (5 min) off</source>
+        <translation>Temporizador (5 min) desactivado</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="102"/>
+        <source>Timer (5 min) on</source>
+        <translation>Temporizador (5 min) activado</translation>
+    </message>
+</context>
+<context>
+    <name>LeagueDialog</name>
+    <message>
+        <location filename="../qml/pages/LeagueDialog.qml" line="48"/>
+        <source>League?</source>
+        <translation>¿Liga?</translation>
+    </message>
+</context>
+<context>
+    <name>MatchInfo</name>
+    <message>
+        <location filename="../qml/pages/MatchInfo.qml" line="62"/>
+        <source>Update</source>
+        <translation>Actualizar</translation>
+    </message>
+</context>
+<context>
+    <name>MatchdayDialog</name>
+    <message>
+        <location filename="../qml/pages/MatchdayDialog.qml" line="14"/>
+        <source>Matchday?</source>
+        <translation>¿Jornada?</translation>
+    </message>
+</context>
+<context>
+    <name>Ranking</name>
+    <message>
+        <location filename="../qml/pages/Ranking.qml" line="31"/>
+        <source>Standings</source>
+        <translation>Clasificación</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Ranking.qml" line="46"/>
+        <source>Update</source>
+        <translation>Actualizar</translation>
+    </message>
+</context>
+<context>
+    <name>Ticker</name>
+    <message>
+        <location filename="../qml/pages/Ticker.qml" line="48"/>
+        <source>Update</source>
+        <translation>Actualizar</translation>
+    </message>
+</context>
+</TS>
