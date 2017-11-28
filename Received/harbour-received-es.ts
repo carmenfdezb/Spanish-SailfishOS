5f74ddcb6edb26ee551b2d60138773512369ad85
Carmen F. B <6286776+carmenfdezb@users.noreply.github.com>
2017-11-28 21:25:51 +0100

    Add files via upload

--- Received/harbour-received-es.ts
+++ Received/harbour-received-es.ts
@@ -1,6 +1,33 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="es_ES">
+<TS version="2.1" language="es_ES">
+<context>
+    <name>ApiLanguageListModel</name>
+    <message>
+        <source>English</source>
+        <translation>Inglés</translation>
+    </message>
+    <message>
+        <source>German</source>
+        <translation>Alemán</translation>
+    </message>
+    <message>
+        <source>Austrian</source>
+        <translation>Austriaco</translation>
+    </message>
+    <message>
+        <source>French</source>
+        <translation>Francés</translation>
+    </message>
+    <message>
+        <source>Portuguese</source>
+        <translation>Portugués</translation>
+    </message>
+    <message>
+        <source>Spanish</source>
+        <translation>Español</translation>
+    </message>
+</context>
 <context>
     <name>BrowseByCategoryPageForm.ui</name>
     <message>
@@ -20,7 +47,7 @@
     </message>
     <message>
         <source>Recommended</source>
-        <translation>Recomendado</translation>
+        <translation>Recomendaciones</translation>
     </message>
     <message>
         <source>Genre</source>
@@ -99,6 +126,14 @@
         <source>Country</source>
         <translation>País</translation>
     </message>
+    <message>
+        <source>Pop</source>
+        <translation>Pop</translation>
+    </message>
+    <message>
+        <source>Genre</source>
+        <translation>Género</translation>
+    </message>
 </context>
 <context>
     <name>FavoritesListContextMenu</name>
@@ -107,8 +142,12 @@
         <translation>Eliminar de favoritos</translation>
     </message>
     <message>
+        <source>Deleting</source>
+        <translation>Borrando</translation>
+    </message>
+    <message>
         <source>Edit favorite</source>
-        <translation>Editar</translation>
+        <translation>Editar favorito</translation>
     </message>
 </context>
 <context>
@@ -116,6 +155,10 @@
     <message>
         <source>Favorites</source>
         <translation>Favoritos</translation>
+    </message>
+    <message>
+        <source>From </source>
+        <translation>De </translation>
     </message>
     <message>
         <source>Add Custom</source>
@@ -142,10 +185,60 @@
     </message>
 </context>
 <context>
+    <name>SettingsPage</name>
+    <message>
+        <source>Droping DB</source>
+        <translation>Borrando BD</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPageForm.ui</name>
+    <message>
+        <source>About</source>
+        <translation>Acerca de</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Ajustes</translation>
+    </message>
+    <message>
+        <source>Advanced Options</source>
+        <translation>Opciones avanzadas</translation>
+    </message>
+    <message>
+        <source>Reset DB</source>
+        <translation>Restaurar BD</translation>
+    </message>
+    <message>
+        <source>&lt;h2&gt;Settings info:&lt;/h2&gt;</source>
+        <translation>&lt;h2&gt;Información sobre los ajustes:&lt;/h2&gt;</translation>
+    </message>
+    <message>
+        <source>&lt;p&gt;All settings are saved when you make a change&lt;/p&gt;</source>
+        <translation>&lt;p&gt;Todos los ajustes se guardan cuando haces un cambio&lt;/p&gt;</translation>
+    </message>
+    <message>
+        <source>&lt;p&gt;&lt;b&gt;Reset DB:&lt;/b&gt; Removes everything in the database and gives you a clean start&lt;/p&gt;</source>
+        <translation>&lt;p&gt;&lt;b&gt;Restaurar BD:&lt;/b&gt; Borra el contenido de la base de datos y te proporciona un inicio limpio&lt;/p&gt;</translation>
+    </message>
+    <message>
+        <source>API Language:</source>
+        <translation>Idioma API:</translation>
+    </message>
+    <message>
+        <source>&lt;p&gt;&lt;b&gt;API Language:&lt;/b&gt; Sets the endpoint to be used for API calls e.g. rad.io for English and radio.de for German&lt;/p&gt;</source>
+        <translation>&lt;p&gt;&lt;b&gt;Idioma API:&lt;/b&gt; Especifica el endpoint usado en las llamadas API, p.e. rad.io para inglés y radio.de para alemán&lt;/p&gt;</translation>
+    </message>
+</context>
+<context>
     <name>StationsListContextMenu</name>
     <message>
         <source>Remove from favorite</source>
         <translation>Eliminar de favoritos</translation>
+    </message>
+    <message>
+        <source>Deleting</source>
+        <translation>Borrando</translation>
     </message>
     <message>
         <source>Add to favorite</source>
@@ -175,7 +268,7 @@
     </message>
     <message>
         <source>Recommended</source>
-        <translation>Recomendado</translation>
+        <translation>Recomendaciones</translation>
     </message>
     <message>
         <source>Local</source>
