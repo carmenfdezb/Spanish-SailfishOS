0ca1a713c81fba06d5dfda0538aeb8a6cf13a656
Carmen F. B <6286776+carmenfdezb@users.noreply.github.com>
2017-11-28 21:31:51 +0100

    Add files via upload

---
+++ Pingviini/harbour-pingviini-es.ts
@@ -0,0 +1,202 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="es" version="2.1">
+<context>
+    <name>AccountAdd</name>
+    <message>
+        <source>Add Account</source>
+        <translation>Añadir cuenta</translation>
+    </message>
+</context>
+<context>
+    <name>Browser</name>
+    <message>
+        <source>Web mode</source>
+        <translation>Modo Web</translation>
+    </message>
+    <message>
+        <source>Reading mode</source>
+        <translation>Modo lectura</translation>
+    </message>
+    <message>
+        <source>Open in Browser</source>
+        <translation>Abrir en el navegador</translation>
+    </message>
+</context>
+<context>
+    <name>CreditsTranslations</name>
+    <message>
+        <source>Translate App</source>
+        <translation>Traducir aplicación</translation>
+    </message>
+    <message>
+        <source>Credits</source>
+        <translation>Créditos</translation>
+    </message>
+</context>
+<context>
+    <name>DirectMessages</name>
+    <message>
+        <source>Messages</source>
+        <translation>Mensajes</translation>
+    </message>
+    <message>
+        <source>Pingviini</source>
+        <translation>Pingviini</translation>
+    </message>
+    <message>
+        <source>Load more</source>
+        <translation>Cargar más</translation>
+    </message>
+</context>
+<context>
+    <name>ImageUploader</name>
+    <message>
+        <source>The file %1 does not exists</source>
+        <translation>El archivo %1 no existe</translation>
+    </message>
+</context>
+<context>
+    <name>MainPage</name>
+    <message>
+        <source>Pingviini</source>
+        <translation>Pingviini</translation>
+    </message>
+    <message>
+        <source>Messages</source>
+        <translation>Mensajes</translation>
+    </message>
+    <message>
+        <source>Timeline</source>
+        <translation>Inicio</translation>
+    </message>
+    <message>
+        <source>Mentions</source>
+        <translation>Menciones</translation>
+    </message>
+    <message>
+        <source>Search</source>
+        <translation>Buscar</translation>
+    </message>
+</context>
+<context>
+    <name>MyList</name>
+    <message>
+        <source>Load more</source>
+        <translation>Cargar más</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Ajustes</translation>
+    </message>
+    <message>
+        <source>Users</source>
+        <translation>Usuarios</translation>
+    </message>
+</context>
+<context>
+    <name>NewTweet</name>
+    <message>
+        <source>Delete</source>
+        <translation>Borrar</translation>
+    </message>
+</context>
+<context>
+    <name>Profile</name>
+    <message>
+        <source>Summary</source>
+        <translation>Resumen</translation>
+    </message>
+    <message>
+        <source>Location</source>
+        <translation>Ubicación</translation>
+    </message>
+    <message>
+        <source>Followers</source>
+        <translation>Seguidores</translation>
+    </message>
+    <message>
+        <source>Following</source>
+        <translation>Siguiendo</translation>
+    </message>
+    <message>
+        <source>Tweets</source>
+        <translation>Tuits</translation>
+    </message>
+    <message>
+        <source>Favourites</source>
+        <translation>Favoritos</translation>
+    </message>
+    <message>
+        <source>Unfollow</source>
+        <translation>Dejar de seguir</translation>
+    </message>
+    <message>
+        <source>Follow</source>
+        <translation>Seguir</translation>
+    </message>
+    <message>
+        <source>Mute</source>
+        <translation>Silenciar</translation>
+    </message>
+    <message>
+        <source>Unmute</source>
+        <translation>Dejar de silenciar</translation>
+    </message>
+</context>
+<context>
+    <name>Settings</name>
+    <message>
+        <source>Settings</source>
+        <translation>Ajustes</translation>
+    </message>
+    <message>
+        <source>Add / Replace Account</source>
+        <translation>Añadir / Reemplazar cuenta</translation>
+    </message>
+    <message>
+        <source>Load images in tweets</source>
+        <translation>Cargar imágenes en tuits</translation>
+    </message>
+    <message>
+        <source>Disable this option if you want to preserve your data connection.</source>
+        <translation>Deshabilita esta opción si quieres ahorrar en tu conexión de datos.</translation>
+    </message>
+    <message>
+        <source>Authorize this app to use your Twitter account in your behalf.</source>
+        <translation>Autoriza a esta aplicación a usar tu cuenta de Twitter en tu nombre.</translation>
+    </message>
+</context>
+<context>
+    <name>Tweet</name>
+    <message>
+        <source>Unfavorite</source>
+        <translation>Eliminar favorito</translation>
+    </message>
+    <message>
+        <source>Favorite</source>
+        <translation>Favorito</translation>
+    </message>
+    <message>
+        <source>Retweet</source>
+        <translation>Retuitear</translation>
+    </message>
+</context>
+<context>
+    <name>TweetDetails</name>
+    <message>
+        <source>Retweet</source>
+        <translation>Retuitear</translation>
+    </message>
+    <message>
+        <source>Unfavorite</source>
+        <translation>Eliminar favorito</translation>
+    </message>
+    <message>
+        <source>Favorite</source>
+        <translation>Favorito</translation>
+    </message>
+    <message>
+        <source>Report as spam</source>
+        <translation>Denunciar como spam</translation>
+    </message>
+</context>
+</TS>
