678e7eb460d64cdc5073ecf0eeed313bce1cd987
Carmen F. B <6286776+carmenfdezb@users.noreply.github.com>
2017-12-24 12:38:27 +0100

    Add files via upload

---
+++ LinksBag/harbour-linksbag-es.ts
@@ -0,0 +1,212 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="es" version="2.1">
+<context>
+    <name>BookmarkViewPage</name>
+    <message>
+        <location filename="../qml/pages/BookmarkViewPage.qml" line="92"/>
+        <source>Mark as unread</source>
+        <translation>Marcar como no leído</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/BookmarkViewPage.qml" line="93"/>
+        <source>Mark as read</source>
+        <translation>Marcar como leído</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/BookmarkViewPage.qml" line="103"/>
+        <source>Mark as unfavorite</source>
+        <translation>No favorito</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/BookmarkViewPage.qml" line="104"/>
+        <source>Mark as favorite</source>
+        <translation>Favorito</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/BookmarkViewPage.qml" line="112"/>
+        <source>Open in browser</source>
+        <translation>Abrir en el navegador</translation>
+    </message>
+</context>
+<context>
+    <name>BookmarksPage</name>
+    <message>
+        <location filename="../qml/pages/BookmarksPage.qml" line="47"/>
+        <source>All</source>
+        <translation>Todo</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/BookmarksPage.qml" line="52"/>
+        <source>Read</source>
+        <translation>Leídos</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/BookmarksPage.qml" line="57"/>
+        <source>Unread</source>
+        <translation>No leídos</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/BookmarksPage.qml" line="62"/>
+        <source>Favorite</source>
+        <translation>Favoritos</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/BookmarksPage.qml" line="101"/>
+        <source>Logout...</source>
+        <translation>Cerrando sesión...</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/BookmarksPage.qml" line="118"/>
+        <source>Bookmarks: %1</source>
+        <translation>Marcadores: %1</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/BookmarksPage.qml" line="129"/>
+        <source>Search</source>
+        <translation>Buscar</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/BookmarksPage.qml" line="141"/>
+        <source>There are no bookmarks. Pull down to refresh.</source>
+        <translation>No hay marcadores. Desliza hacia abajo para actualizar.</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/BookmarksPage.qml" line="148"/>
+        <source>Logout</source>
+        <translation>Cerrar sesión</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/BookmarksPage.qml" line="157"/>
+        <source>Hide search field</source>
+        <translation>Ocultar campo de búsqueda</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/BookmarksPage.qml" line="158"/>
+        <source>Show search field</source>
+        <translation>Mostrar campo de búsqueda</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/BookmarksPage.qml" line="169"/>
+        <source>View: %1</source>
+        <translation>Mostrar : %1</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/BookmarksPage.qml" line="178"/>
+        <source>Refresh</source>
+        <translation>Actualizar</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/BookmarksPage.qml" line="197"/>
+        <source>Copy url to clipboard</source>
+        <translation>Copiar url al portapapeles</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/BookmarksPage.qml" line="200"/>
+        <source>Url copied into clipboard</source>
+        <translation>Url copiada al portapapeles</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/BookmarksPage.qml" line="205"/>
+        <source>Open in browser</source>
+        <translation>Abrir en el navegador</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/BookmarksPage.qml" line="212"/>
+        <source>Edit tags</source>
+        <translation>Editar etiquetas</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/BookmarksPage.qml" line="222"/>
+        <location filename="../qml/pages/BookmarksPage.qml" line="316"/>
+        <source>Remove</source>
+        <translation>Eliminar</translation>
+    </message>
+</context>
+<context>
+    <name>EditTagDialog</name>
+    <message>
+        <location filename="../qml/pages/EditTagDialog.qml" line="41"/>
+        <source>Save</source>
+        <translation>Guardar</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/EditTagDialog.qml" line="42"/>
+        <source>Cancel</source>
+        <translation>Cancelar</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/EditTagDialog.qml" line="50"/>
+        <source>Tags</source>
+        <translation>Etiquetas</translation>
+    </message>
+</context>
+<context>
+    <name>FilterSelectorPage</name>
+    <message>
+        <location filename="../qml/pages/FilterSelectorPage.qml" line="46"/>
+        <source>Filter by</source>
+        <translation>Filtro</translation>
+    </message>
+</context>
+<context>
+    <name>LinksBag::GetPocketApi</name>
+    <message>
+        <location filename="../src/getpocketapi.cpp" line="227"/>
+        <source>General error</source>
+        <translation>Error general</translation>
+    </message>
+    <message>
+        <location filename="../src/getpocketapi.cpp" line="243"/>
+        <source>Network error %1: %2</source>
+        <translation>Error de red: %1: %2</translation>
+    </message>
+    <message>
+        <location filename="../src/getpocketapi.cpp" line="266"/>
+        <source>Reply data is corrupted</source>
+        <translation>Los datos de respuesta están corruptos</translation>
+    </message>
+    <message>
+        <location filename="../src/getpocketapi.cpp" line="378"/>
+        <source>Unable to remove bookamark</source>
+        <translation>No es posible eliminar marcador</translation>
+    </message>
+    <message>
+        <location filename="../src/getpocketapi.cpp" line="400"/>
+        <location filename="../src/getpocketapi.cpp" line="423"/>
+        <source>Unable to mark bookamark as %1</source>
+        <translation>No es posible marcar marcador como %1</translation>
+    </message>
+    <message>
+        <location filename="../src/getpocketapi.cpp" line="401"/>
+        <source>favorite</source>
+        <translation>favorito</translation>
+    </message>
+    <message>
+        <location filename="../src/getpocketapi.cpp" line="401"/>
+        <source>unfavorite</source>
+        <translation>no favorito</translation>
+    </message>
+    <message>
+        <location filename="../src/getpocketapi.cpp" line="424"/>
+        <source>read</source>
+        <translation>leído</translation>
+    </message>
+    <message>
+        <location filename="../src/getpocketapi.cpp" line="424"/>
+        <source>unread</source>
+        <translation>no leído</translation>
+    </message>
+    <message>
+        <location filename="../src/getpocketapi.cpp" line="446"/>
+        <source>Unable to update tags</source>
+        <translation>No es posible actualizar etiquetas</translation>
+    </message>
+</context>
+<context>
+    <name>LinksBag::LinksBagManager</name>
+    <message>
+        <location filename="../src/linksbagmanager.cpp" line="99"/>
+        <source>GetPocket error (%1): </source>
+        <translation>Error en servivo pocket (%1): </translation>
+    </message>
+</context>
+</TS>
