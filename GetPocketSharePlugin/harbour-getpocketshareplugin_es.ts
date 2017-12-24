39e3ff3aefc6f760db9b9206ed165280e437c8e8
Carmen F. B <6286776+carmenfdezb@users.noreply.github.com>
2017-12-24 18:49:03 +0100

    Add files via upload

---
+++ GetPocketSharePlugin/harbour-getpocketshareplugin_es.ts
@@ -0,0 +1,41 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="es" version="2.1">
+<context>
+    <name>GetPocketMediaTransfer</name>
+    <message>
+        <location filename="../src/getpocketmediatransfer.cpp" line="71"/>
+        <source>Bookmark added succesfully</source>
+        <translation>Marcador añadido con éxito</translation>
+    </message>
+    <message>
+        <location filename="../src/getpocketmediatransfer.cpp" line="84"/>
+        <source>Bookmark adding failed</source>
+        <translation>Error al añadir marcador</translation>
+    </message>
+</context>
+<context>
+    <name>GetPocketPluginInfo</name>
+    <message>
+        <location filename="../src/getpocketplugininfo.cpp" line="29"/>
+        <source>LinksBag not authorized</source>
+        <translation>LinksBag no autorizado</translation>
+    </message>
+</context>
+<context>
+    <name>GetPocketShareUi</name>
+    <message>
+        <location filename="../qml/GetPocketShareUi.qml" line="20"/>
+        <source>Add</source>
+        <translation>Añadir</translation>
+    </message>
+    <message>
+        <location filename="../qml/GetPocketShareUi.qml" line="49"/>
+        <source>Tags</source>
+        <translation>Etiquetas</translation>
+    </message>
+    <message>
+        <location filename="../qml/GetPocketShareUi.qml" line="50"/>
+        <source>Tags (separated by comma)...</source>
+        <translation>Etiquetas (separadas por coma)...</translation>
+    </message>
+</context>
+</TS>
