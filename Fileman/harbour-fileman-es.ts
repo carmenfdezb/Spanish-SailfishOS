09ca1e15b5ad4e18a1ab30e2eaa09560161990c3
Carmen F. B <carmenfdezb@mail.com>
2019-03-28 09:47:59 +0100

    Add files via upload

---
+++ Fileman/harbour-fileman-es.ts
@@ -0,0 +1,284 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="es_ES">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <source>What&apos;s</source>
+        <translation>¿Qué es</translation>
+    </message>
+    <message>
+        <source>Licensing</source>
+        <translation>Licencia</translation>
+    </message>
+    <message>
+        <source>is free software released under the GNU General Public License (GPL), version 3 or later.</source>
+        <translation>es un programa libre publicado bajo la Licencia Pública General de GNU (GPL), versión 3 o posterior.</translation>
+    </message>
+    <message>
+        <source>Rudi Timmermans</source>
+        <translation>Rudi Timmermans</translation>
+    </message>
+    <message>
+        <source>Donate with Paypal</source>
+        <translation>Donar con Paypal</translation>
+    </message>
+    <message>
+        <source>Version</source>
+        <translation>Versión</translation>
+    </message>
+    <message>
+        <source>Fileman is a File Manager for Sailfish OS.</source>
+        <translation>Fileman es un explorador de archivos para Sailfish OS.</translation>
+    </message>
+    <message>
+        <source>Fileman developer</source>
+        <translation>Desarrollador de Fileman</translation>
+    </message>
+    <message>
+        <source>About Fileman</source>
+        <translation>Acerca de Fileman</translation>
+    </message>
+    <message>
+        <source>Icons</source>
+        <translation>Iconos</translation>
+    </message>
+    <message>
+        <source>Fileman icons made by Molan and levone1.</source>
+        <translation>Iconos de Fileman realizados por Molan y levone1.</translation>
+    </message>
+</context>
+<context>
+    <name>DirectoryFileOperations</name>
+    <message>
+        <source>Rename</source>
+        <translation>Renombrar</translation>
+    </message>
+    <message>
+        <source>Cut</source>
+        <translation>Cortar</translation>
+    </message>
+    <message>
+        <source>Copy</source>
+        <translation>Copiar</translation>
+    </message>
+    <message>
+        <source>Paste</source>
+        <translation>Pegar</translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation>Borrar</translation>
+    </message>
+    <message>
+        <source>Compress</source>
+        <translation>Comprimir</translation>
+    </message>
+    <message>
+        <source>Share</source>
+        <translation>Compartir</translation>
+    </message>
+</context>
+<context>
+    <name>DirectoryPullDownMenu</name>
+    <message>
+        <source>Settings</source>
+        <translation>Ajustes</translation>
+    </message>
+    <message>
+        <source>New...</source>
+        <translation>Nuevo...</translation>
+    </message>
+    <message>
+        <source>Show hidden files</source>
+        <translation>Mostrar archivos ocultos</translation>
+    </message>
+    <message>
+        <source>Don&apos;t show hidden files</source>
+        <translation>No mostrar archivos ocultos</translation>
+    </message>
+    <message>
+        <source>Places</source>
+        <translation>Lugares</translation>
+    </message>
+</context>
+<context>
+    <name>DirectoryPushUpMenu</name>
+    <message>
+        <source>Add to bookmarks</source>
+        <translation>Añadir a favoritos</translation>
+    </message>
+    <message>
+        <source>Remove from bookmarks</source>
+        <translation>Eliminar de favoritos</translation>
+    </message>
+    <message>
+        <source>Directory properties</source>
+        <translation>Propiedades del directorio</translation>
+    </message>
+</context>
+<context>
+    <name>DirectoryViewSettings</name>
+    <message>
+        <source>Directory view</source>
+        <translation>Apariencia de directorio</translation>
+    </message>
+</context>
+<context>
+    <name>FileDisplay</name>
+    <message>
+        <source>File display</source>
+        <translation>Visualización de archivos</translation>
+    </message>
+    <message>
+        <source>File display look</source>
+        <translation>Apariencia en visualización de archivos</translation>
+    </message>
+    <message>
+        <source>Browse through all files</source>
+        <translation>Explorar todos los archivos</translation>
+    </message>
+    <message>
+        <source>When viewing a file, browse through all viewable files instead of files with the same file type</source>
+        <translation>Al visualizar un archivo, explorar todos los archivos visibles en lugar de los archivos del mismo tipo</translation>
+    </message>
+</context>
+<context>
+    <name>FileInfo</name>
+    <message>
+        <source>File information</source>
+        <translation>Información de archivo</translation>
+    </message>
+</context>
+<context>
+    <name>PermissionEdit</name>
+    <message>
+        <source>Read</source>
+        <translation>Lectura</translation>
+    </message>
+    <message>
+        <source>Write</source>
+        <translation>Escritura</translation>
+    </message>
+    <message>
+        <source>Execute</source>
+        <translation>Ejecución</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <source>Settings</source>
+        <translation>Ajustes</translation>
+    </message>
+    <message>
+        <source>Directory view</source>
+        <translation>Apariencia de directorio</translation>
+    </message>
+    <message>
+        <source>File ordering</source>
+        <translation>Orden de archivos</translation>
+    </message>
+    <message>
+        <source>File display</source>
+        <translation>Visualización de archivos</translation>
+    </message>
+    <message>
+        <source>General</source>
+        <translation>General</translation>
+    </message>
+    <message>
+        <source>Fileman</source>
+        <translation>Fileman</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>Acerca de</translation>
+    </message>
+    <message>
+        <source>Translations</source>
+        <translation>Traducciones</translation>
+    </message>
+</context>
+<context>
+    <name>ShortcutsView</name>
+    <message>
+        <source>Last location</source>
+        <translation>Última ubicación</translation>
+    </message>
+    <message>
+        <source>Documents</source>
+        <translation>Documentos</translation>
+    </message>
+    <message>
+        <source>Downloads</source>
+        <translation>Descargas</translation>
+    </message>
+    <message>
+        <source>Music</source>
+        <translation>Música</translation>
+    </message>
+    <message>
+        <source>Pictures</source>
+        <translation>Imágenes</translation>
+    </message>
+    <message>
+        <source>Videos</source>
+        <translation>Vídeos</translation>
+    </message>
+    <message>
+        <source>Locations</source>
+        <translation>Ubicaciones</translation>
+    </message>
+    <message>
+        <source>Android storage</source>
+        <translation>Almacenamiento de Android</translation>
+    </message>
+    <message>
+        <source>Bookmarks</source>
+        <translation>Favoritos</translation>
+    </message>
+    <message>
+        <source>Storage devices</source>
+        <translation>Dispositivos de almacenamiento</translation>
+    </message>
+    <message>
+        <source>SD card</source>
+        <translation>Tarjeta SD</translation>
+    </message>
+    <message>
+        <source>Places</source>
+        <translation>Lugares</translation>
+    </message>
+    <message>
+        <source>Home</source>
+        <translation>Carpeta de usuario</translation>
+    </message>
+</context>
+<context>
+    <name>TextEdit</name>
+    <message>
+        <source>Go back</source>
+        <translation>Ir atrás</translation>
+    </message>
+    <message>
+        <source>Clear</source>
+        <translation>Limpiar</translation>
+    </message>
+    <message>
+        <source>Save</source>
+        <translation>Guardar</translation>
+    </message>
+</context>
+<context>
+    <name>TranslationsPage</name>
+    <message>
+        <source>Fileman translators</source>
+        <translation>Traductores de Fileman</translation>
+    </message>
+    <message>
+        <source>Translation Platform</source>
+        <translation>Plataforma de traducción</translation>
+    </message>
+</context>
+</TS>
