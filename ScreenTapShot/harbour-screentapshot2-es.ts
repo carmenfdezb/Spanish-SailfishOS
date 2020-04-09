8211fd86f92aafcf4e6da77f1f1725403c224c88
Carmen <carmen@localhost.localdomain>
2020-04-09 19:28:56 +0200

    0.3.0-1

---
+++ ScreenTapShot/harbour-screentapshot2-es.ts
@@ -0,0 +1,137 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="es" version="2.1">
+<context>
+    <name/>
+    <message>
+        <location filename="../qml/AboutPage.qml" line="182"/>
+        <source>ScreenTapShot</source>
+        <extracomment>Application name, affect icon text in the app grid</extracomment>
+        <translation>ScreenTapShot</translation>
+    </message>
+</context>
+<context>
+    <name>AboutPage</name>
+    <message>
+        <location filename="../qml/AboutPage.qml" line="169"/>
+        <source>About</source>
+        <extracomment>Header text</extracomment>
+        <translation>Acerca de</translation>
+    </message>
+    <message>
+        <location filename="../qml/AboutPage.qml" line="207"/>
+        <source>Simple screenshot application with overlay button</source>
+        <extracomment>About text</extracomment>
+        <translation>Aplicación sencilla de capturas de pantalla con botón superpuesto</translation>
+    </message>
+    <message>
+        <location filename="../qml/AboutPage.qml" line="216"/>
+        <source>Thanks to tortoisedoc for MOUSE_REGION trick.</source>
+        <extracomment>About thanks</extracomment>
+        <translation>Gracias a tortoisec por el truco de MOUSE_REGION.</translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <location filename="../qml/CoverPage.qml" line="7"/>
+        <source>Screen
+Tap
+Shot
+Settings</source>
+        <extracomment>Cover text</extracomment>
+        <translation>Ajustes de
+Screen
+Tap
+Shot</translation>
+    </message>
+</context>
+<context>
+    <name>MainPage</name>
+    <message>
+        <location filename="../qml/MainPage.qml" line="16"/>
+        <source>Close overlay</source>
+        <extracomment>Pulldown menu item</extracomment>
+        <translation>Cerrar superposición</translation>
+    </message>
+    <message>
+        <location filename="../qml/MainPage.qml" line="21"/>
+        <source>About</source>
+        <extracomment>Pulldown menu item</extracomment>
+        <translation>Acerca de</translation>
+    </message>
+    <message>
+        <location filename="../qml/MainPage.qml" line="32"/>
+        <source>ScreenTapShot</source>
+        <extracomment>Page header</extracomment>
+        <translation>ScreenTapShot</translation>
+    </message>
+    <message>
+        <location filename="../qml/MainPage.qml" line="37"/>
+        <source>Screenshot animation</source>
+        <extracomment>Switch to toggle animation after screenshot</extracomment>
+        <translation>Animación en captura</translation>
+    </message>
+    <message>
+        <location filename="../qml/MainPage.qml" line="45"/>
+        <source>Screenshot delay</source>
+        <extracomment>Slider to change delay before taking screenshot</extracomment>
+        <translation>Retraso en captura</translation>
+    </message>
+    <message>
+        <location filename="../qml/MainPage.qml" line="51"/>
+        <source>No delay</source>
+        <extracomment>Slider value text if delay set to 0</extracomment>
+        <translation>Sin retraso</translation>
+    </message>
+    <message>
+        <location filename="../qml/MainPage.qml" line="53"/>
+        <source>%1s</source>
+        <extracomment>Slider value text</extracomment>
+        <translation>%1s</translation>
+    </message>
+    <message>
+        <location filename="../qml/MainPage.qml" line="65"/>
+        <source>With no delay you can close overlay only using settings page pulldown menu.</source>
+        <extracomment>Helper text</extracomment>
+        <translation>Con la opción &quot;sin retraso&quot;, sólo puedes cerrar la superposición desde los ajustes en el menú deslizante.</translation>
+    </message>
+    <message>
+        <location filename="../qml/MainPage.qml" line="67"/>
+        <source>When delay is set you can close overlay with doubletap on capture icon</source>
+        <extracomment>Helper text</extracomment>
+        <translation>Al configurar un tiempo de retraso puedes cerrar la superposición con un doble toque sobre el icono de captura</translation>
+    </message>
+    <message>
+        <location filename="../qml/MainPage.qml" line="75"/>
+        <source>Save in Screenshots subfolder</source>
+        <extracomment>Switch to enable saving in Screenshots subfolder</extracomment>
+        <translation>Guardar en subcarpeta Screenshots</translation>
+    </message>
+</context>
+<context>
+    <name>overlay</name>
+    <message>
+        <location filename="../qml/overlay.qml" line="288"/>
+        <source>Application removal</source>
+        <extracomment>Title of overlay visible while removing still launched application</extracomment>
+        <translation>Eliminación de la aplicación</translation>
+    </message>
+    <message>
+        <location filename="../qml/overlay.qml" line="382"/>
+        <source>I&apos;m sorry You unsatisfied with my application. Please tell me why, and I will try to do my best to improve it.</source>
+        <extracomment>Removal overlay text</extracomment>
+        <translation>Lamento que no estés satisfecho con mi aplicación. Por favor, dime el motivo e intentaré hacer todo lo posible para mejorarla.</translation>
+    </message>
+    <message>
+        <location filename="../qml/overlay.qml" line="388"/>
+        <source>Leave comment in OpenRepos</source>
+        <extracomment>Removal overlay button to open openrepos in browser</extracomment>
+        <translation>Dejar comentario en OpenRepos</translation>
+    </message>
+    <message>
+        <location filename="../qml/overlay.qml" line="400"/>
+        <source>No, thanks</source>
+        <extracomment>Removal overlay button to close application</extracomment>
+        <translation>No, gracias</translation>
+    </message>
+</context>
+</TS>
