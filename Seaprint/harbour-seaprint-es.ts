74478634f23914c45c63ace8b2e9a5930d499312
Carmen <carmen@localhost.localdomain>
2020-07-12 17:21:50 +0200

    v0.5.2

---
+++ Seaprint/harbour-seaprint-es.ts
@@ -0,0 +1,576 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="es">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <source>GitHub repository</source>
+        <translation>Repositorio en Github</translation>
+    </message>
+    <message>
+        <source>Report an Issue</source>
+        <translation>Informar de un problema</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>Acerca de</translation>
+    </message>
+    <message>
+        <source>Network printing for Sailfish OS.</source>
+        <translation>Impresión por red para Sailfish OS.</translation>
+    </message>
+    <message>
+        <source>Icons</source>
+        <translation>Iconos</translation>
+    </message>
+    <message>
+        <source>Icon made by Gregguh.</source>
+        <translation>Icono creado por Gregguh.</translation>
+    </message>
+    <message>
+        <source>General contributions</source>
+        <translation>Colaboración general</translation>
+    </message>
+    <message>
+        <source>Translations</source>
+        <translation>Traducciones</translation>
+    </message>
+    <message>
+        <source>Chinese</source>
+        <translation>Chino</translation>
+    </message>
+    <message>
+        <source>Licensing</source>
+        <translation>Licencia</translation>
+    </message>
+    <message>
+        <source>Source code is available at GitHub. Translations, bug reports and other contributions are welcome!</source>
+        <translation>El código fuente está disponible en Github. ¡Las traducciones, informar sobre errores y otras colaboraciones son bienvenidas!</translation>
+    </message>
+    <message>
+        <source>SeaPrint licencing is still TBD, but will be some flavor of open.</source>
+        <translation>La licencia de Seaprint aún está por determinar, pero será algo abierta.</translation>
+    </message>
+    <message>
+        <source>French</source>
+        <translation>Francés</translation>
+    </message>
+    <message>
+        <source>Code and Testing - Rudi Timmermans</source>
+        <translation>Código y pruebas - Rudi Timmermans</translation>
+    </message>
+    <message>
+        <source>Spanish</source>
+        <translation>Español</translation>
+    </message>
+    <message>
+        <source>Polish</source>
+        <translation>Polaco</translation>
+    </message>
+    <message>
+        <source>Optional dependencies</source>
+        <translation>Dependencias opcionales</translation>
+    </message>
+    <message>
+        <source>Installed</source>
+        <translation>Instalado</translation>
+    </message>
+    <message>
+        <source>Not installed</source>
+        <translation>No instalado</translation>
+    </message>
+</context>
+<context>
+    <name>AddPrinterDialog</name>
+    <message>
+        <source>Add printer</source>
+        <translation>Añadir impresora</translation>
+    </message>
+    <message>
+        <source>On WiFI:</source>
+        <translation>En WiFi:</translation>
+    </message>
+    <message>
+        <source>Found:</source>
+        <translation>Encontrada:</translation>
+    </message>
+    <message>
+        <source>No printer found</source>
+        <translation>Ninguna impresora encontrada</translation>
+    </message>
+    <message>
+        <source>Unknown</source>
+        <translation>Desconocida</translation>
+    </message>
+</context>
+<context>
+    <name>BusyPage</name>
+    <message>
+        <source>Print success: </source>
+        <translation>Impresión correcta: </translation>
+    </message>
+    <message>
+        <source>Print failed: </source>
+        <translation>Impresión fallida: </translation>
+    </message>
+    <message>
+        <source>Unknown</source>
+        <translation>Desconocido</translation>
+    </message>
+</context>
+<context>
+    <name>ConvertWorker</name>
+    <message>
+        <source>Conversion error</source>
+        <translation>Error de conversión</translation>
+    </message>
+    <message>
+        <source>Failed to load image</source>
+        <translation>Error al cargar imagen</translation>
+    </message>
+    <message>
+        <source>Unsupported PDF paper size</source>
+        <translation>Tamaño de papel en PDF no soportado</translation>
+    </message>
+    <message>
+        <source>Unsupported paper size</source>
+        <translation>Tamaño de papel no soportado</translation>
+    </message>
+    <message>
+        <source>Unsupported resolution (dpi)</source>
+        <translation>Resolución (ppp) no soportada</translation>
+    </message>
+    <message>
+        <source>Unsupported target format</source>
+        <translation>Formato de destino no soportado</translation>
+    </message>
+    <message>
+        <source>Failed to get info about PDF file</source>
+        <translation>Error al obtener info de archivo PDF</translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>SeaPrint</source>
+        <translation>SeaPrint</translation>
+    </message>
+</context>
+<context>
+    <name>FirstPage</name>
+    <message>
+        <source>Add by URL</source>
+        <translation>Añadir por URL</translation>
+    </message>
+    <message>
+        <source>URL</source>
+        <translation>URL</translation>
+    </message>
+    <message>
+        <source>Refresh</source>
+        <translation>Actualizar</translation>
+    </message>
+    <message>
+        <source>No file selected</source>
+        <translation>Ningún archivo seleccionado</translation>
+    </message>
+    <message>
+        <source>View jobs</source>
+        <translation>Ver trabajos</translation>
+    </message>
+    <message>
+        <source>Remove printer</source>
+        <translation>Eliminar impresora</translation>
+    </message>
+    <message>
+        <source>Removing printer</source>
+        <translation>Eliminando impresora</translation>
+    </message>
+    <message>
+        <source>About SeaPrint</source>
+        <translation>Acerca de SeaPrint</translation>
+    </message>
+    <message>
+        <source>Unknown</source>
+        <translation>Desconocido</translation>
+    </message>
+    <message>
+        <source>No compatible formats supported</source>
+        <translation>No admite formatos compatibles</translation>
+    </message>
+    <message>
+        <source>Choose file</source>
+        <translation>Elegir archivo</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Ajustes</translation>
+    </message>
+</context>
+<context>
+    <name>IppPrinter</name>
+    <message>
+        <source>Failed to open file</source>
+        <translation>Error al abrir archivo</translation>
+    </message>
+    <message>
+        <source>Unsupported print media</source>
+        <translation>Medio impreso no soportado</translation>
+    </message>
+    <message>
+        <source>Cannot convert this file format</source>
+        <translation>No se puede convertir este formato de archivo</translation>
+    </message>
+    <message>
+        <source>Unknown document format</source>
+        <translation>Formato de documento desconocido</translation>
+    </message>
+</context>
+<context>
+    <name>JobsPage</name>
+    <message>
+        <source>Refresh</source>
+        <translation>Actualizar</translation>
+    </message>
+    <message>
+        <source>jobs</source>
+        <translation>trabajos</translation>
+    </message>
+    <message>
+        <source>Untitled job</source>
+        <translation>Trabajo sin título</translation>
+    </message>
+    <message>
+        <source>Cancel job</source>
+        <translation>Cancelar trabajo</translation>
+    </message>
+    <message>
+        <source>Cancelling job</source>
+        <translation>Cancelando trabajo</translation>
+    </message>
+    <message>
+        <source>job</source>
+        <translation>trabajo</translation>
+    </message>
+    <message>
+        <source>Cancelling job failed</source>
+        <translation>Cancelando trabajo fallido</translation>
+    </message>
+</context>
+<context>
+    <name>NagScreen</name>
+    <message>
+        <source>Dismiss</source>
+        <translation>Descartar</translation>
+    </message>
+    <message>
+        <source>Don&apos;t show again</source>
+        <translation>No mostrar de nuevo</translation>
+    </message>
+    <message>
+        <source>Optional dependencies are not installed!</source>
+        <translation>¡Las dependencias opcionales no están instaladas!</translation>
+    </message>
+    <message>
+        <source>In order to convert PDF files to other formats, you need utilities from the package &quot;poppler-utils&quot;</source>
+        <translation>Para convertir los archivos PDF a otros formatos necesitas las utilidades del paquete &quot;poppler-utils&quot;</translation>
+    </message>
+    <message>
+        <source>To install, enable delveloper mode, and open a terminal.</source>
+        <translation>Para instalar, habilita el modo de desarollador y abre Terminal.</translation>
+    </message>
+    <message>
+        <source>Become root:</source>
+        <translation>Acceder como root:</translation>
+    </message>
+    <message>
+        <source>Install poppler-utils:</source>
+        <translation>Instalar poppler-utils:</translation>
+    </message>
+</context>
+<context>
+    <name>PrinterPage</name>
+    <message>
+        <source>Print</source>
+        <translation>Imprimir</translation>
+    </message>
+    <message>
+        <source>Sides</source>
+        <translation>Caras</translation>
+    </message>
+    <message>
+        <source>Copies</source>
+        <translation>Copias</translation>
+    </message>
+    <message>
+        <source>Color mode</source>
+        <translation>Modo de color</translation>
+    </message>
+    <message>
+        <source>Quality</source>
+        <translation>Calidad</translation>
+    </message>
+    <message>
+        <source>Resolution</source>
+        <translation>Resolución</translation>
+    </message>
+    <message>
+        <source>Print media</source>
+        <translation>Medio impreso</translation>
+    </message>
+    <message>
+        <source>Transfer format</source>
+        <translation>Formato de transferencia</translation>
+    </message>
+    <message>
+        <source>Media source</source>
+        <translation>Fuente de medios</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <source>Show nag-screen</source>
+        <translation>Mostrar pantalla</translation>
+    </message>
+    <message>
+        <source>Consider additional formats</source>
+        <translation>Considerar formatos adicionales</translation>
+    </message>
+    <message>
+        <source>Some printers support more formats than they advertise correctly. However, additional formats can be inferred from other attributes. Mainly relevant for Postscript-compatible printers that also support PDF.</source>
+        <translation>Algunas impresoras admiten más formatos de los que anuncian. Sin embargo, es posible deducir formatos adicionales a partir de otros atributos. Principalmente es relevante para impresoras compatibles con Postscript que también admiten PDF.</translation>
+    </message>
+    <message>
+        <source>Always convert to raster format</source>
+        <translation>Convertir siempre a formato ráster</translation>
+    </message>
+    <message>
+        <source>Force conversion to PWG/URF raster format. This is mainly intended for testing.</source>
+        <translation>Forzar conversión a formato ráster PWG/URF. Esto es para hacer pruebas principalmente.</translation>
+    </message>
+    <message>
+        <source>Force-include document format</source>
+        <translation>Forzar incluisión de formato de documento</translation>
+    </message>
+    <message>
+        <source>Force the document-format attribute to be included. Some printers have trouble recognizing certain formats, sometimes it helps to include this atribute even if the printer does not claim to support it.</source>
+        <translation>Forzar la inclusión del atributo de formato de documento. Algunas impresoras tienen problemas para reconocer ciertos formatos, a veces es útil incluir este atributo aunque la impresora no asegure que lo soporte.</translation>
+    </message>
+    <message>
+        <source>Remove redundant attributes</source>
+        <translation>Eliminar atributos redundantes</translation>
+    </message>
+    <message>
+        <source>Remove redundant IPP attributes, if they are also conveyed in the transfer format. Some printers reject the job even if the settings are consistent.</source>
+        <translation>Elimina los atributos IPP redundantes, si también se transmiten en el formato de transferencia. Algunas impresoras rechazan el trabajo aunque la configuración sea coherente.</translation>
+    </message>
+    <message>
+        <source>Display the warning page about optional dependencies not being installed, if they are not installed.</source>
+        <translation>Mostrar página de advertencia sobre las dependencias opcionales no instaladas cuando no están instaladas.</translation>
+    </message>
+</context>
+<context>
+    <name>utils</name>
+    <message>
+        <source>pending</source>
+        <translation>pendiente</translation>
+    </message>
+    <message>
+        <source>pending-held</source>
+        <translation>mantiene-pendiente</translation>
+    </message>
+    <message>
+        <source>processing</source>
+        <translation>procesando</translation>
+    </message>
+    <message>
+        <source>processing-stopped</source>
+        <translation>procesando-detenido</translation>
+    </message>
+    <message>
+        <source>canceled</source>
+        <translation>cancelado</translation>
+    </message>
+    <message>
+        <source>aborted</source>
+        <translation>abortado</translation>
+    </message>
+    <message>
+        <source>completed</source>
+        <translation>completado</translation>
+    </message>
+    <message>
+        <source>unknown state </source>
+        <translation>estado desconocido </translation>
+    </message>
+    <message>
+        <source>draft</source>
+        <translation>borrador</translation>
+    </message>
+    <message>
+        <source>normal</source>
+        <translation>normal</translation>
+    </message>
+    <message>
+        <source>high</source>
+        <translation>alta</translation>
+    </message>
+    <message>
+        <source>unknown quality </source>
+        <translation>calidad desconocida </translation>
+    </message>
+    <message>
+        <source>portrait</source>
+        <translation>vertical</translation>
+    </message>
+    <message>
+        <source>landscape</source>
+        <translation>horizontal</translation>
+    </message>
+    <message>
+        <source>reverse landscape</source>
+        <translation>horizontal invertido</translation>
+    </message>
+    <message>
+        <source>reverse portrait</source>
+        <translation>vertical invertido</translation>
+    </message>
+    <message>
+        <source>unknown orientation </source>
+        <translation>orientación desconocida </translation>
+    </message>
+    <message>
+        <source>dpi</source>
+        <translation>dpi</translation>
+    </message>
+    <message>
+        <source>dots/cm</source>
+        <translation>ppp</translation>
+    </message>
+    <message>
+        <source>PDF</source>
+        <translation>PDF</translation>
+    </message>
+    <message>
+        <source>Postscript</source>
+        <translation>Postscript</translation>
+    </message>
+    <message>
+        <source>PWG-raster</source>
+        <translation>ráster-PWG</translation>
+    </message>
+    <message>
+        <source>URF-raster</source>
+        <translation>ráster-URF</translation>
+    </message>
+    <message>
+        <source>PNG</source>
+        <translation>PNG</translation>
+    </message>
+    <message>
+        <source>JPEG</source>
+        <translation>JPEG</translation>
+    </message>
+    <message>
+        <source>GIF</source>
+        <translation>GIF</translation>
+    </message>
+    <message>
+        <source>auto-sense</source>
+        <translation>auto-detección</translation>
+    </message>
+    <message>
+        <source>by-pass tray</source>
+        <translation>bandeja bypass</translation>
+    </message>
+    <message>
+        <source>tray</source>
+        <translation>bandeja</translation>
+    </message>
+    <message>
+        <source>bottom</source>
+        <translation>inferior</translation>
+    </message>
+    <message>
+        <source>center</source>
+        <translation>centro</translation>
+    </message>
+    <message>
+        <source>main</source>
+        <translation>principal</translation>
+    </message>
+    <message>
+        <source>manual</source>
+        <translation>manual</translation>
+    </message>
+    <message>
+        <source>middle</source>
+        <translation>medio</translation>
+    </message>
+    <message>
+        <source>photo</source>
+        <translation>foto</translation>
+    </message>
+    <message>
+        <source>rear</source>
+        <translation>posterior</translation>
+    </message>
+    <message>
+        <source>right</source>
+        <translation>derecho</translation>
+    </message>
+    <message>
+        <source>side</source>
+        <translation>cara</translation>
+    </message>
+    <message>
+        <source>top</source>
+        <translation>superior</translation>
+    </message>
+    <message>
+        <source>automatic</source>
+        <translation>automático</translation>
+    </message>
+    <message>
+        <source>auto monochrome</source>
+        <translation>auto blanco y negro</translation>
+    </message>
+    <message>
+        <source>bi-level</source>
+        <translation>dos niveles</translation>
+    </message>
+    <message>
+        <source>color</source>
+        <translation>color</translation>
+    </message>
+    <message>
+        <source>highlight</source>
+        <translation>resaltado</translation>
+    </message>
+    <message>
+        <source>monochrome</source>
+        <translation>blanco y negro</translation>
+    </message>
+    <message>
+        <source>process bi-level</source>
+        <translation>proceso dos niveles</translation>
+    </message>
+    <message>
+        <source>process monochrome</source>
+        <translation>proceso blanco y negro</translation>
+    </message>
+    <message>
+        <source>one-sided</source>
+        <translation>una cara</translation>
+    </message>
+    <message>
+        <source>two-sided</source>
+        <translation>dos caras</translation>
+    </message>
+    <message>
+        <source>two-sided flipped</source>
+        <translation>doble cara volteado</translation>
+    </message>
+</context>
+</TS>
