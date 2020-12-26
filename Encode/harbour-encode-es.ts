7865516f575851ecddc1644b4ecfc0f5589edc25
Carmen Fdez. B <6286776+carmenfdezb@users.noreply.github.com>
2020-12-26 17:46:15 +0100

    v1.4-1

--- Encode/harbour-encode-es.ts
+++ Encode/harbour-encode-es.ts
@@ -1,6 +1,14 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="es_ES">
+<TS version="2.1" language="es_ES">
+<context>
+    <name></name>
+    <message id="components_gallery-la-image-loading-failed">
+        <source>Oops, can&apos;t display the image</source>
+        <extracomment>Image loading failed</extracomment>
+        <translation>Ups, no se puede mostrar la imagen</translation>
+    </message>
+</context>
 <context>
     <name>AboutPage</name>
     <message>
@@ -20,6 +28,29 @@
     <message>
         <source>My Cover</source>
         <translation>Mi cubierta</translation>
+    </message>
+</context>
+<context>
+    <name>CreateDirDialog</name>
+    <message>
+        <source>Error creating new directory</source>
+        <translation>Error al crear nuevo directorio</translation>
+    </message>
+    <message>
+        <source>Create Folder</source>
+        <translation>Crear carpeta</translation>
+    </message>
+    <message>
+        <source>Create</source>
+        <translation>Crear</translation>
+    </message>
+    <message>
+        <source>Create a new folder under</source>
+        <translation>Crear una carpeta nueva bajo</translation>
+    </message>
+    <message>
+        <source>Folder name</source>
+        <translation>Nombre de carpeta</translation>
     </message>
 </context>
 <context>
@@ -71,6 +102,88 @@
     <message>
         <source>Aspect:</source>
         <translation>Aspecto:</translation>
+    </message>
+</context>
+<context>
+    <name>FileItemDelegate</name>
+    <message>
+        <source>Deleting </source>
+        <translation>Borrando </translation>
+    </message>
+    <message>
+        <source>Select</source>
+        <translation>Seleccionar</translation>
+    </message>
+    <message>
+        <source>Cut</source>
+        <translation>Cortar</translation>
+    </message>
+    <message>
+        <source>Copy</source>
+        <translation>Copiar</translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation>Borrar</translation>
+    </message>
+    <message>
+        <source>Properties</source>
+        <translation>Propiedades</translation>
+    </message>
+</context>
+<context>
+    <name>FileProperties</name>
+    <message>
+        <source>Open with Texteditor</source>
+        <translation>Abrir con editor de texto</translation>
+    </message>
+    <message>
+        <source>Change Permissions</source>
+        <translation>Cambiar permisos</translation>
+    </message>
+    <message>
+        <source>Rename</source>
+        <translation>Renombrar</translation>
+    </message>
+    <message>
+        <source>Error renaming</source>
+        <translation>Error al renombrar</translation>
+    </message>
+    <message>
+        <source>Properties</source>
+        <translation>Propiedades</translation>
+    </message>
+    <message>
+        <source>Location</source>
+        <translation>Ubicación</translation>
+    </message>
+    <message>
+        <source>Type</source>
+        <translation>Tipo</translation>
+    </message>
+    <message>
+        <source>Link to %1</source>
+        <translation>Enlace a %1</translation>
+    </message>
+    <message>
+        <source>Size</source>
+        <translation>Tamaño</translation>
+    </message>
+    <message>
+        <source>Permissions</source>
+        <translation>Permisos</translation>
+    </message>
+    <message>
+        <source>Owner</source>
+        <translation>Propietario</translation>
+    </message>
+    <message>
+        <source>Group</source>
+        <translation>Grupo</translation>
+    </message>
+    <message>
+        <source>Last modified</source>
+        <translation>Últ. modificación</translation>
     </message>
 </context>
 <context>
@@ -145,6 +258,24 @@ Archivo guardado en </translation>
     </message>
 </context>
 <context>
+    <name>ImageControls</name>
+    <message>
+        <source>Open externally</source>
+        <translation>Abrir externamente</translation>
+    </message>
+    <message>
+        <source>Opening...</source>
+        <translation>Abriendo...</translation>
+    </message>
+</context>
+<context>
+    <name>MultiSelectBar</name>
+    <message>
+        <source>Deleting %1 files</source>
+        <translation>Borrando %1 archivos</translation>
+    </message>
+</context>
+<context>
     <name>OpenDialog</name>
     <message>
         <source>Open file</source>
@@ -175,28 +306,261 @@ Archivo guardado en </translation>
         <translation>Error al pegar archivo </translation>
     </message>
     <message>
-        <source>Show Filesystem Root</source>
-        <translation>Mostrar sistema de archivos raíz</translation>
-    </message>
-    <message>
-        <source>Show Home</source>
-        <translation>Mostrar Home</translation>
-    </message>
-    <message>
-        <source>Show Android SDCard</source>
-        <translation>Mostrar tarjeta SD Android</translation>
-    </message>
-    <message>
-        <source>Show SDCard</source>
-        <translation>Mostrar tarjeta SD</translation>
-    </message>
-    <message>
         <source>directory</source>
         <translation>directorio</translation>
     </message>
     <message>
         <source>File operation succeeded</source>
-        <translation>Operación de archivo correcta</translation>
+        <translation>Operación de archivo completada</translation>
+    </message>
+    <message>
+        <source>Name</source>
+        <translation>Nombre</translation>
+    </message>
+    <message>
+        <source>Time</source>
+        <translation>Hora</translation>
+    </message>
+    <message>
+        <source>Size</source>
+        <translation>Tamaño</translation>
+    </message>
+    <message>
+        <source>Type</source>
+        <translation>Tipo</translation>
+    </message>
+    <message>
+        <source>Sort by: </source>
+        <translation>Ordenar por: </translation>
+    </message>
+    <message>
+        <source>Add to places</source>
+        <translation>Añadir a lugares</translation>
+    </message>
+    <message>
+        <source>Properties</source>
+        <translation>Ptopiedades</translation>
+    </message>
+    <message>
+        <source> Files</source>
+        <translation> archivos</translation>
+    </message>
+    <message>
+        <source>Error deleting file(s)</source>
+        <translation>Error al borrar archivo(s)</translation>
+    </message>
+    <message>
+        <source>File deletion succeeded</source>
+        <translation>Eliminación de archivo completada</translation>
+    </message>
+</context>
+<context>
+    <name>OpenURLPage</name>
+    <message>
+        <source>Load URL</source>
+        <translation>Cargar URL</translation>
+    </message>
+    <message>
+        <source>Type in URL here</source>
+        <translation>Escribe aquí la URL</translation>
+    </message>
+    <message>
+        <source>URL to media file/stream</source>
+        <translation>URL a archivo multimedia/transmisión</translation>
+    </message>
+</context>
+<context>
+    <name>PermissionDialog</name>
+    <message>
+        <source>Could not change permission</source>
+        <translation>No es posible cambiar permiso</translation>
+    </message>
+    <message>
+        <source>Change Permissions</source>
+        <translation>Cambiar permisos</translation>
+    </message>
+    <message>
+        <source>Change</source>
+        <translation>Cambiar</translation>
+    </message>
+    <message>
+        <source>Change permissions for</source>
+        <translation>Cambiar permisos de</translation>
+    </message>
+    <message>
+        <source>Read</source>
+        <translation>Lectura</translation>
+    </message>
+    <message>
+        <source>Write</source>
+        <translation>Escritura</translation>
+    </message>
+    <message>
+        <source>Execute</source>
+        <translation>Ejecución</translation>
+    </message>
+    <message>
+        <source>Owner</source>
+        <translation>Propietario</translation>
+    </message>
+    <message>
+        <source>Group</source>
+        <translation>Grupo</translation>
+    </message>
+    <message>
+        <source>Others</source>
+        <translation>Otros</translation>
+    </message>
+</context>
+<context>
+    <name>PlacesPage</name>
+    <message>
+        <source>Device memory</source>
+        <translation>Memoria de dispositivo</translation>
+    </message>
+    <message>
+        <source>SD Card</source>
+        <translation>Tarjeta SD</translation>
+    </message>
+    <message>
+        <source>Home</source>
+        <translation>Carpeta de usuario</translation>
+    </message>
+    <message>
+        <source>Documents</source>
+        <translation>Documentos</translation>
+    </message>
+    <message>
+        <source>Downloads</source>
+        <translation>Descargas</translation>
+    </message>
+    <message>
+        <source>Music</source>
+        <translation>Música</translation>
+    </message>
+    <message>
+        <source>Pictures</source>
+        <translation>Imágenes</translation>
+    </message>
+    <message>
+        <source>Videos</source>
+        <translation>Vídeos</translation>
+    </message>
+    <message>
+        <source>Android Storage</source>
+        <translation>Almacenamiento Android</translation>
+    </message>
+    <message>
+        <source>Places</source>
+        <translation>Lugares</translation>
+    </message>
+    <message>
+        <source>Search</source>
+        <translation>Buscar</translation>
+    </message>
+    <message>
+        <source>Devices</source>
+        <translation>Dispositivos</translation>
+    </message>
+    <message>
+        <source>Common</source>
+        <translation>Común</translation>
+    </message>
+    <message>
+        <source>Custom</source>
+        <translation>Personalizado</translation>
+    </message>
+    <message>
+        <source>Deleting </source>
+        <translation>Borrando </translation>
+    </message>
+    <message>
+        <source>Rename</source>
+        <translation>Renombrar</translation>
+    </message>
+</context>
+<context>
+    <name>RenameDialog</name>
+    <message>
+        <source>Rename</source>
+        <translation>Renombrar</translation>
+    </message>
+    <message>
+        <source>Give a new name for</source>
+        <translation>Asigna un nombre nuevo para</translation>
+    </message>
+    <message>
+        <source>New name</source>
+        <translation>Nuevo nombre</translation>
+    </message>
+</context>
+<context>
+    <name>TextEditor</name>
+    <message>
+        <source>Save</source>
+        <translation>Guardar</translation>
+    </message>
+</context>
+<context>
+    <name>VideoPlayer</name>
+    <message>
+        <source>Open File</source>
+        <translation>Abrir archivo</translation>
+    </message>
+    <message>
+        <source>Open Stream</source>
+        <translation>Abrir transmisión</translation>
+    </message>
+</context>
+<context>
+    <name>VideoPlayerComponent</name>
+    <message>
+        <source>Pull down to load file or stream</source>
+        <translation>Desliza hacia abajo para cargar archivo o transmisión</translation>
+    </message>
+    <message>
+        <source>Dismiss</source>
+        <translation>Cancelar</translation>
+    </message>
+    <message>
+        <source>Zoomed to fit screen</source>
+        <translation>Ajustado a la pantalla</translation>
+    </message>
+    <message>
+        <source>Original</source>
+        <translation>Original</translation>
+    </message>
+    <message>
+        <source>
+The video cannot be played due to a problem allocating resources.
+On Youtube Videos please make sure to be logged in. Some videos might be geoblocked or require you to be logged into youtube.</source>
+        <translation>
+No se puede reproducir vídeo por un problema de asignación de recursos.
+Asegúrate de haber iniciado sesión para los vídeos de Youtube. Algunos vídeos pueden estar bloqueados geográficamente o requieren inicio de sesión.</translation>
+    </message>
+    <message>
+        <source>
+The audio and or video format is not supported.</source>
+        <translation>
+Formato de audio y vídeo no soportado.</translation>
+    </message>
+    <message>
+        <source>
+The video cannot be played due to network issues.</source>
+        <translation>
+No se puede reproducir vídeo por problemas de conexión.</translation>
+    </message>
+    <message>
+        <source>
+The video cannot be played due to insufficient permissions.</source>
+        <translation>
+No se puede reproducir vídeo debido a permisos insuficientes.</translation>
+    </message>
+    <message>
+        <source>
+The video cannot be played because the media service could not be instantiated.</source>
+        <translation>
+No se puede reproducir vídeo porque no se pudo instanciar el servicio multimedia.</translation>
     </message>
 </context>
 </TS>
