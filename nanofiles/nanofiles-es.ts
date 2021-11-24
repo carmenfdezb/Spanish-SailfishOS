d869e2c2b649032208ad54f1f9d9d54a37adfcfa
Carmen Fdez. B <6286776+carmenfdezb@users.noreply.github.com>
2021-11-24 21:05:32 +0100

    v1.1-1

---
+++ nanofiles/nanofiles-es.ts
@@ -0,0 +1,301 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="es_ES">
+<context>
+    <name>About</name>
+    <message>
+        <source>About</source>
+        <translation>Acerca de</translation>
+    </message>
+    <message>
+        <source>Made by Michał Szczepaniak. Based on fileman.</source>
+        <translation>Realizado por Michał Szczepaniak. Basado en fileman.</translation>
+    </message>
+    <message>
+        <source>Licensed under GPLv3</source>
+        <translation>Bajo licencia GPLv3</translation>
+    </message>
+    <message>
+        <source>Links</source>
+        <translation>Enlaces</translation>
+    </message>
+    <message>
+        <source>Thanks to</source>
+        <translation>Gracias a</translation>
+    </message>
+    <message>
+        <source>Rudi Timmermans for fileman.</source>
+        <translation>Rudi Timmermans por fileman.</translation>
+    </message>
+    <message>
+        <source>Donate</source>
+        <translation>Donar</translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>Nanofiles</source>
+        <translation>Nanofiles</translation>
+    </message>
+</context>
+<context>
+    <name>ErrorDialog</name>
+    <message>
+        <source>Error</source>
+        <translation>Error</translation>
+    </message>
+    <message>
+        <source>Okay</source>
+        <translation>Bien</translation>
+    </message>
+</context>
+<context>
+    <name>FileInfo</name>
+    <message>
+        <source>Share</source>
+        <translation>Compartir</translation>
+    </message>
+    <message>
+        <source>Size:</source>
+        <translation>Tamaño:</translation>
+    </message>
+    <message>
+        <source>Modified:</source>
+        <translation>Modificado:</translation>
+    </message>
+    <message>
+        <source>Accessed:</source>
+        <translation>Último acceso:</translation>
+    </message>
+</context>
+<context>
+    <name>FileProcess</name>
+    <message>
+        <source>Installing...</source>
+        <translation>Instalando...</translation>
+    </message>
+    <message>
+        <source>Opening file...</source>
+        <translation>Abriendo archivo...</translation>
+    </message>
+    <message>
+        <source>Installing packages</source>
+        <translation>Instalando paquetes</translation>
+    </message>
+    <message>
+        <source>Downloading packages</source>
+        <translation>Descargando paquetes</translation>
+    </message>
+    <message>
+        <source>Installed</source>
+        <translation>Instalado</translation>
+    </message>
+    <message>
+        <source>Nothing was done</source>
+        <translation>No se ha realizado nada</translation>
+    </message>
+    <message>
+        <source>Installation failed</source>
+        <translation>Error en la instalación</translation>
+    </message>
+    <message>
+        <source>Installation was started</source>
+        <translation>Se ha iniciado la instalación</translation>
+    </message>
+    <message>
+        <source>Installation couldn&apos;t be started</source>
+        <translation>No se pudo iniciar la instalación</translation>
+    </message>
+    <message>
+        <source>File opened</source>
+        <translation>Archivo abierto</translation>
+    </message>
+    <message>
+        <source>Suitable application was not found</source>
+        <translation>No se ha encontrado la aplicación adecuada</translation>
+    </message>
+    <message>
+        <source>File couldn&apos;t be opened</source>
+        <translation>No se pudo abrir el archivo</translation>
+    </message>
+</context>
+<context>
+    <name>FileRenameDialog</name>
+    <message>
+        <source>Rename</source>
+        <translation>Renombrar</translation>
+    </message>
+</context>
+<context>
+    <name>Main</name>
+    <message>
+        <source>About</source>
+        <translation>Acerca de</translation>
+    </message>
+    <message>
+        <source>New…</source>
+        <translation>Nuevo...</translation>
+    </message>
+    <message>
+        <source>Add to places</source>
+        <translation>Añadir a lugares</translation>
+    </message>
+    <message>
+        <source>Hide hidden files</source>
+        <translation>No mostrar archivos ocultos</translation>
+    </message>
+    <message>
+        <source>Show hidden files</source>
+        <translation>Mostrar archivos ocultos</translation>
+    </message>
+    <message>
+        <source>Switch to grid view</source>
+        <translation>Cambiar a vista de cuadrícula</translation>
+    </message>
+    <message>
+        <source>Switch to list view</source>
+        <translation>Cambiar a vista de lista</translation>
+    </message>
+    <message>
+        <source>Restart in user mode</source>
+        <translation>Reiniciar en modo usuario</translation>
+    </message>
+    <message>
+        <source>Restart in root mode</source>
+        <translation>Reiniciar en modo root</translation>
+    </message>
+    <message>
+        <source>Size: %1 Modified: %2</source>
+        <translation>Tamaño: %1 Modificado: %2</translation>
+    </message>
+</context>
+<context>
+    <name>NewFilesDialog</name>
+    <message>
+        <source>Directory</source>
+        <translation>Directorio</translation>
+    </message>
+    <message>
+        <source>File</source>
+        <translation>Archivo</translation>
+    </message>
+    <message>
+        <source>Type</source>
+        <translation>Tipo</translation>
+    </message>
+    <message>
+        <source>Name…</source>
+        <translation>Nombre...</translation>
+    </message>
+</context>
+<context>
+    <name>Places</name>
+    <message>
+        <source>Delete</source>
+        <translation>Eliminar</translation>
+    </message>
+    <message>
+        <source>Move Up</source>
+        <translation>Subir</translation>
+    </message>
+    <message>
+        <source>Move Down</source>
+        <translation>Bajar</translation>
+    </message>
+    <message>
+        <source>Rename</source>
+        <translation>Renombrar</translation>
+    </message>
+    <message>
+        <source>Change name</source>
+        <translation>Cambiar nombre</translation>
+    </message>
+    <message>
+        <source>Reset entries</source>
+        <translation>Reiniciar entradas</translation>
+    </message>
+</context>
+<context>
+    <name>ShareFilesPage</name>
+    <message>
+        <source>No sharing accounts available!</source>
+        <translation>¡No hay cuentas disponibles para compartir!</translation>
+    </message>
+    <message>
+        <source>Share</source>
+        <translation>Compartir</translation>
+    </message>
+</context>
+<context>
+    <name>Worker</name>
+    <message>
+        <source>Copying files...</source>
+        <translation>Copiando archivos...</translation>
+    </message>
+    <message>
+        <source>Creating directories...</source>
+        <translation>Creando directorios...</translation>
+    </message>
+    <message>
+        <source>Directories couldn&apos;t be created.</source>
+        <translation>No se pudieron crear los directorios.</translation>
+    </message>
+    <message>
+        <source>Copying files (%1 of %2)...</source>
+        <translation>Copiando archivos (%1 de %2)...</translation>
+    </message>
+    <message>
+        <source>All files were copied successfully.</source>
+        <translation>Todos los archivos se copiaron correctamente.</translation>
+    </message>
+    <message>
+        <source>All files couldn&apos;t be copied successfully.</source>
+        <translation>No se pudieron copiar correctamente todos los archivos.</translation>
+    </message>
+    <message>
+        <source>Deleting old files (%1 of %2)...</source>
+        <translation>Eliminando archivos antiguos (%1 de %2)...</translation>
+    </message>
+    <message>
+        <source>Deleting old directories...</source>
+        <translation>Eliminando directorios antiguos...</translation>
+    </message>
+    <message>
+        <source>All of the files were cut and pasted successfully.</source>
+        <translation>Todos los archivos se cortaron y copiaron correctamente.</translation>
+    </message>
+    <message>
+        <source>All of the files couldn&apos;t be cut and pasted successfully.</source>
+        <translation>No se pudieron cortar y pegar correctamente todos los archivos.</translation>
+    </message>
+    <message>
+        <source>Failed to copy all files, deleting copied files (%1 of %2)...</source>
+        <translation>Error al copiar todos los archivos, borrando archivos copiados (%1 de %2)...</translation>
+    </message>
+    <message>
+        <source>Failed to copy all files, deleting created directories...</source>
+        <translation>Error al copiar todos los archivos. eliminando directorios creados...</translation>
+    </message>
+    <message>
+        <source>Deleting files...</source>
+        <translation>Eliminando archivos...</translation>
+    </message>
+    <message>
+        <source>Deleting files (%1 of %2)...</source>
+        <translation>Eliminando archivos (%1 de %2)...</translation>
+    </message>
+    <message>
+        <source>Deleting directories...</source>
+        <translation>Eliminando directorios...</translation>
+    </message>
+    <message>
+        <source>All files were deleted successfully.</source>
+        <translation>Todos los archivos se eliminaron correctamente.</translation>
+    </message>
+    <message>
+        <source>All files couldn&apos;t be deleted successfully.</source>
+        <translation>No se pudieron eliminar correctamente todos los archivos.</translation>
+    </message>
+</context>
+</TS>
