c430e833a4a58a790cf7d561f95a84e0c58004fc
Carmen Fdez. B <6286776+carmenfdezb@users.noreply.github.com>
2022-01-03 22:17:39 +0100

    v0.1.7

--- Aenigma/harbour-aenigma-es.ts
+++ Aenigma/harbour-aenigma-es.ts
@@ -216,5 +216,10 @@
         <source>Hints used</source>
         <translation>Sugerencias usadas</translation>
     </message>
+    <message id="id-generator-failed-msg">
+        <source>Generation of Sudoku game failed! Please try again!</source>
+        <oldsource>Generation of game failed! Please try again!</oldsource>
+        <translation>¡La generación del sudoku ha fallado! ¡Inténtalo de nuevo!</translation>
+    </message>
 </context>
 </TS>
