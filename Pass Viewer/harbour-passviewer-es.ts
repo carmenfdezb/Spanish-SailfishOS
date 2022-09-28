86a8623f23ef5ff99e3d1c6883867e7d8caceae9
Carmen Fdez. B <6286776+carmenfdezb@users.noreply.github.com>
2022-09-29 00:26:42 +0200

    v1.5-2

---
+++ Pass Viewer/harbour-passviewer-es.ts
@@ -0,0 +1,155 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="es_ES">
+<context>
+    <name>FirstPage</name>
+    <message>
+        <source>No passes found</source>
+        <translation>No hay pases</translation>
+    </message>
+    <message>
+        <source>Copyright</source>
+        <translation>Derechos de autor</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Ajustes</translation>
+    </message>
+    <message>
+        <source>Show</source>
+        <translation>Mostrar</translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation>Borrar</translation>
+    </message>
+    <message>
+        <source>Deleting</source>
+        <translation>Borrando</translation>
+    </message>
+    <message>
+        <source>Update</source>
+        <translation>Actualizar</translation>
+    </message>
+    <message>
+        <source>pass update successful</source>
+        <translation>pase actualizado correctamente</translation>
+    </message>
+    <message>
+        <source>pass not updateable</source>
+        <translation>pase no actualizable</translation>
+    </message>
+    <message>
+        <source>no new version for pass</source>
+        <translation>no hay actualizaciones del pase</translation>
+    </message>
+    <message>
+        <source>pass update failed</source>
+        <translation>error al actualizar pase</translation>
+    </message>
+    <message>
+        <source>Format Error</source>
+        <translation>Error de formato</translation>
+    </message>
+    <message>
+        <source>Couldn&apos;t recognize date/time format</source>
+        <translation>No es posible reconocer el formato de fecha/hora</translation>
+    </message>
+    <message>
+        <source>Unsupported</source>
+        <translation>No soportado</translation>
+    </message>
+    <message>
+        <source>Please update your system or install calendar</source>
+        <translation>Por favor, actualiza tu sistema o instala Calendario</translation>
+    </message>
+    <message>
+        <source>Create Calendar Entry</source>
+        <translation>Crear entrada en Calendario</translation>
+    </message>
+</context>
+<context>
+    <name>Settings</name>
+    <message>
+        <source>Time</source>
+        <translation>Hora</translation>
+    </message>
+    <message>
+        <source>Highlight passes close to event time</source>
+        <translation>Resaltar pases cuya hora del evento va a finalizar</translation>
+    </message>
+    <message>
+        <source>Time before event</source>
+        <translation>Horas antes del evento</translation>
+    </message>
+    <message>
+        <source>Time after event</source>
+        <translation>Horas después del evento</translation>
+    </message>
+    <message>
+        <source>Distance</source>
+        <translation>Distancia</translation>
+    </message>
+    <message>
+        <source>Highlight passes close to destination</source>
+        <translation>Resaltar pases cuyo destino está próximo</translation>
+    </message>
+    <message>
+        <source>Distance to destination</source>
+        <translation>Distancia al destino</translation>
+    </message>
+    <message>
+        <source>Allow passes to override distance</source>
+        <translation>Permitir pases que no cumplan la distancia</translation>
+    </message>
+    <message>
+        <source>Barcode</source>
+        <translation>Código de barras</translation>
+    </message>
+    <message>
+        <source>Show barcode fullscreen when tapped</source>
+        <translation>Mostrar al pulsar el código de barras a pantalla completa</translation>
+    </message>
+    <message>
+        <source>Prefer non-satellite position fixing</source>
+        <translation>No usar preferiblemente el satélite para obtener la ubicación</translation>
+    </message>
+    <message>
+        <source>Saves battery. &lt;b&gt;Requires&lt;/b&gt; &amp;quot;Faster position fix&amp;quot; switched on in the system settings.</source>
+        <translation>Ahorra batería. &lt;b&gt;Es neceesario activar la opción&lt;/b&gt; &amp;quot;Posicionamiento de gran precisión&amp;quot; en la configuración del sistema.</translation>
+    </message>
+</context>
+<context>
+    <name>ShowBack</name>
+    <message>
+        <source>Simple View</source>
+        <translation>Vista simple</translation>
+    </message>
+</context>
+<context>
+    <name>ShowPass</name>
+    <message>
+        <source>Fullscreen Barcode</source>
+        <translation>Código de barras a pantalla completa</translation>
+    </message>
+    <message>
+        <source>Simple View</source>
+        <translation>Vista simple</translation>
+    </message>
+    <message>
+        <source>Create Calendar Entry</source>
+        <translation>Crear entrada en Calendario</translation>
+    </message>
+    <message>
+        <source>Update</source>
+        <translation>Actualizar</translation>
+    </message>
+</context>
+<context>
+    <name>ShowSimple</name>
+    <message>
+        <source>Fullscreen Barcode</source>
+        <translation>Código de barras a pantalla completa</translation>
+    </message>
+</context>
+</TS>
